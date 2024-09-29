16fa388dc226b8b95778a5a9f663c06548c461ba
tun67213 <tun67213@temple.edu>
2024-09-29 12:10:40 -0400

    Added next()

--- Iterator.java
+++ Iterator.java
@@ -9,4 +9,9 @@ public interface Iterator<E>
      * @return true if the iteration has more elements
      */
     boolean hasNext();
+
+    /**
+     * @return the next element in the iteration
+     */
+    E next();
 }
