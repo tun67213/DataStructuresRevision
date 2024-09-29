3d75bfc8c6abd3586b4190d2e3f2ef34e90e794d
tun67213 <tun67213@temple.edu>
2024-09-29 12:15:05 -0400

    Included add()

--- ListIterator.java
+++ ListIterator.java
@@ -5,5 +5,10 @@
  */
 public interface ListIterator<E> extends Iterator<E>
 {
-
+    /**
+     * @param e the value to add to the end of the List
+     * @throws NullPointerException if no value is provided
+     * @code Inserts the specified element into the list.
+     */
+    void add(E e);
 }
