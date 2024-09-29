435b9cdcd38c9b9d118e39a4d916bbd5a7a8c1bc
tun67213 <tun67213@temple.edu>
2024-09-29 18:16:25 -0400

    Included add() functions
    Included add(E e) and addAll(Collection<? extends E> c).

--- Collection.java
+++ Collection.java
@@ -4,5 +4,18 @@
  */
 public interface Collection<E> extends Iterable<E>
 {
+    /**
+     * @param e the value to add to the end of this Collection
+     * @throws NullPointerException if no value is provided or the provided value is 'null'
+     * @return true if the Collection has accepted the provided value
+     * @code Ensures that this collection contains the specified element.
+     */
+    boolean add(E e);
 
+    /**
+     * @param c Collection of values to add to the end of this Collection
+     * @throws NullPointerException if no Collection is provided or the provided Collection variable is 'null' or any value in the provided Collection is 'null'
+     * @return false if the provided Collection is empty, true if all the values have been added to the end of this Collection, in the order returned by this Collection's iterator
+     */
+    boolean addAll(Collection<? extends E> c);
 }
