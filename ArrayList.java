dc9477e0ab9b8c229a48991f9cf023a08c6881aa
tun67213 <tun67213@temple.edu>
2024-09-29 12:08:02 -0400

    Set up file with generics

---
+++ ArrayList.java
@@ -0,0 +1,8 @@
+/**
+ * @author arvindhvelrajan
+ * @param <E> Generic variable for this class
+ */
+public class ArrayList<E>
+{
+
+}
---
+++ LinkedList.java
@@ -0,0 +1,8 @@
+/**
+ * @author arvindhvelrajan
+ * @param <E> Generic variable for this class
+ */
+public class LinkedList<E>
+{
+    
+}
