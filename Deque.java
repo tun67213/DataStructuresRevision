debe719044f6d8a1c639f39784a5f61f9a337b2b
tun67213 <tun67213@temple.edu>
2024-09-30 14:19:56 -0400

    Included addFirst() and addLast()

--- Deque.java
+++ Deque.java
@@ -12,4 +12,18 @@ public interface Deque<E> extends Queue<E>
      */
     @Override
     boolean add(E e);
+
+    /**
+     * @param e the value to add to the beginning of this Deque
+     * @throws NullPointerException if no element is provided or the provided element is null
+     * @code Inserts the specified element at the front of this deque if it is possible to do so immediately without violating capacity restrictions
+     */
+    void addFirst(E e);
+
+    /**
+     * @param e the value to add to the end of this Deque
+     * @throws NullPointerException if no Object is provided or the provided element is null
+     * @code Inserts the specified element at the end of this deque if it is possible to do so immediately without violating capacity restrictions
+     */
+    void addLast(E e);
 }
