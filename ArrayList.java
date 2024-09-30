538aca2cc8f7e5c2ec3f2cf8a53d5bf8c13b02e8
tun67213 <tun67213@temple.edu>
2024-09-30 07:09:47 -0400

    Added the three constructors

--- ArrayList.java
+++ ArrayList.java
@@ -7,4 +7,31 @@ public class ArrayList<E>
     private int size;
     private int capacity;
     private E[] values;
+
+    /**
+     * @code Default constructor
+     */
+    public ArrayList()
+    {
+
+    }
+
+    /**
+     * @param c Collection of values to initialize this ArrayList with
+     * @throws NullPointerException if no Collection variable is provided or any value in the provided Collection is 'null'
+     * @code Initializes this ArrayList with values from the provided Collection
+     */
+    public ArrayList(Collection<? extends E> c)
+    {
+
+    }
+
+    /**
+     * @param initialCapacity the number of elements the array representing this ArrayList should initially hold
+     * @code Initializes this ArrayList by creating an array that is of 'initialCapacity' elements
+     */
+    public ArrayList(int initialCapacity)
+    {
+        
+    }
 }
