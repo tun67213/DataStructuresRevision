db7b9148ae45fdad33b4dd9fff1e82f702252828
tun67213 <tun67213@temple.edu>
2024-09-29 20:27:37 -0400

    Included both add() functions

--- List.java
+++ List.java
@@ -5,5 +5,20 @@
  */
 public interface List<E> extends Collection<E>
 {
-    
+    /**
+     * @param e the value to be added to the end of this List
+     * @throws NullPointerException if no value is provided or the provided value is 'null'
+     * @return true if this List has been modified (If not, the function will have already throws an exception)
+     */
+    @Override
+    boolean add(E e);
+
+    /**
+     * @param index the location at which to add the specified element
+     * @param element the value to add to the specified index in this List
+     * @throws NullPointerException if no value is provided
+     * @throws IndexOutOfBoundsException if the provided index is out of bounds
+     * @code Inserts the specified element at the specified position in this list.
+     */
+    boolean add(int index, E element);
 }
