d8d52cc740a9e9680c3d6d145c2a534de7d3408f
tun67213 <tun67213@temple.edu>
2024-09-30 10:11:54 -0400

    Imported modules necessary for implementing class

--- ArrayList.java
+++ ArrayList.java
@@ -3,7 +3,7 @@
  * @author arvindhvelrajan
  * @param <E> Generic variable for this class
  */
-public class ArrayList<E>
+public class ArrayList<E> extends AbstractList<E> implements List<E>, RandomAccess, Cloneable, Serializable
 {
     
 }
