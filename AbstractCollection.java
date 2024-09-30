92ac11524547e9aa0a407ddfe768ae7aab0b80d2
tun67213 <tun67213@temple.edu>
2024-09-30 07:16:15 -0400

    Importing functions from Collection

--- AbstractCollection.java
+++ AbstractCollection.java
@@ -3,7 +3,7 @@
  * @author arvindhvelrajan
  * @param <E> Generic variable for this abstract class
  */
-public abstract class AbstractCollection<E>
+public abstract class AbstractCollection<E> implements Collection<E>
 {
     
 }
