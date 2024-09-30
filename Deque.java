afb014dd5509a2fa5a768f83060a76fb90aab8f6
tun67213 <tun67213@temple.edu>
2024-09-30 14:17:10 -0400

    Included add()

--- Deque.java
+++ Deque.java
@@ -5,5 +5,11 @@
  */
 public interface Deque<E> extends Queue<E>
 {
-    
+    /**
+     * @param e the value to be added to this Deque
+     * @throws NullPointerException if no Object is provided or the provided Object is 'null'
+     * @return true if adding was successful
+     */
+    @Override
+    boolean add(E e);
 }
