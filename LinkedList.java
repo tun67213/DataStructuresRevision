33ecc994b7ddd11c5ff214b15a8193f7ca16fdfd
tun67213 <tun67213@temple.edu>
2024-09-30 17:24:26 -0400

    Imported modules necessary for implementing class

--- LinkedList.java
+++ LinkedList.java
@@ -2,7 +2,7 @@
  * @author arvindhvelrajan
  * @param <E> Generic variable for this class
  */
-public class LinkedList<E>
+public class LinkedList<E> extends AbstractSequentialList<E> implements List<E>, Deque<E>, Cloneable, Serializable
 {
 
 }
