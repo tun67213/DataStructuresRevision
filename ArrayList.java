9ba7ece2547d8c85040b01c5c33753fb32e58847
tun67213 <tun67213@temple.edu>
2024-09-30 12:54:25 -0400

    Added size()

--- ArrayList.java
+++ ArrayList.java
@@ -276,4 +276,13 @@ public class ArrayList<E> extends AbstractList<E> implements List<E>, RandomAcce
     {
         throw new UnsupportedOperationException("Not supported yet.");
     }
+
+    /**
+     * @return the number of elements in this ArrayList
+     */
+    @Override
+    public int size()
+    {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
 }
