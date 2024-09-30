d3a6f22fe510b4f555368fd05ef57cadccf38648
tun67213 <tun67213@temple.edu>
2024-09-30 08:30:34 -0400

    Added constructor

--- AbstractList.java
+++ AbstractList.java
@@ -5,5 +5,16 @@
  */
 public abstract class AbstractList<E> extends AbstractCollection<E> implements List<E>
 {
+    /**
+     * @code Counts the number of times this list has been structurally modified
+     */
+    protected int modCount;
 
+    /**
+     * @code Sole constructor.
+     */
+    protected AbstractList()
+    {
+
+    }
 }
