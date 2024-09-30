69282cded5519b4833406145fd92fd4fea3ab0dd
tun67213 <tun67213@temple.edu>
2024-09-30 07:14:19 -0400

    Included add() statements

--- ArrayList.java
+++ ArrayList.java
@@ -40,6 +40,30 @@ public class ArrayList<E>
      */
     private void ensureCapacity()
     {
-        
+
+    }
+
+    /**
+     * @param e the value to be added to the end of this ArrayList
+     * @throws NullPointerException if no value is provided or the provided value is 'null'
+     * @return true if the value was added to the end of this ArrayList successfully
+     */
+    @Override
+    public boolean add(E e)
+    {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    /**
+     * @param index the location at which to insert the specified element in this ArrayList
+     * @param element the value to be added to the specified location in this ArrayList
+     * @throws NullPointerException if no element variable is provided or the provided variable is 'null'
+     * @throws IndexOutOfBoundsException if the provided index is out of bounds
+     * @code If no element is provided or the provided element is null, the function throws a NullPointerException(). If the provided index is out of bounds, the function throws an IndexOutOfBoundsException(). Otherwise, it shifts elements to the right and allocates space for this element.
+     */
+    @Override
+    public void add(int index, E element)
+    {
+        throw new UnsupportedOperationException("Not supported yet.");
     }
 }
