ab3e403fc1182c8fe1a099b0477f5492a33d2f44
tun67213 <tun67213@temple.edu>
2024-09-30 17:28:59 -0400

    Included constructors

--- LinkedList.java
+++ LinkedList.java
@@ -5,4 +5,22 @@
 public class LinkedList<E> extends AbstractSequentialList<E> implements List<E>, Deque<E>, Cloneable, Serializable
 {
     private int modCount;   // Counts the number of structural changes that occur to this LinkedList
+
+    /**
+     * @code Constructs an empty list.
+     */
+    public LinkedList()
+    {
+
+    }
+
+    /**
+     * @param c Collection of values to initialize this LinkedList with
+     * @throws NullPointerException if the provided Collection variable is null or any value in the provided Collection is null
+     * @code Constructs a list containing the elements of the specified collection, in the order they are returned by the collection's iterator.
+     */
+    public LinkedList(Collection<? extends E> c)
+    {
+        
+    }
 }
