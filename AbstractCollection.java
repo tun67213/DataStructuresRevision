d840898e654647a2890e4ce486956a8a575de1ad
tun67213 <tun67213@temple.edu>
2024-09-30 07:19:53 -0400

    Included 'add' methods

--- AbstractCollection.java
+++ AbstractCollection.java
@@ -12,4 +12,27 @@ public abstract class AbstractCollection<E> implements Collection<E>
     {
 
     }
+
+    /**
+     * @param e the value to be added to this Collection
+     * @throws NullPointerException if no value is provided or the provided value is 'null'
+     * @return true if this collection contains the specified element
+     */
+    @Override
+    public boolean add(E e)
+    {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * @param c Collection of values to add to the end of this Collection
+     * @throws NullPointerException if the provided Collection is null or any value in it is null
+     * @return true if this AbstractCollection has changed
+     * @code Adds all the elements in the specified collection to this collection.
+     */
+    @Override
+    public boolean addAll(Collection<? extends E> c)
+    {
+        throw new UnsupportedOperationException();
+    }
 }
