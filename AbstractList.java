8f52f49ef2022e0e8f11ce95018847275c806d9e
tun67213 <tun67213@temple.edu>
2024-09-30 08:34:18 -0400

    Included add() statements

--- AbstractList.java
+++ AbstractList.java
@@ -17,4 +17,42 @@ public abstract class AbstractList<E> extends AbstractCollection<E> implements L
     {
 
     }
+
+    /**
+     * @param e the value to add to the end of this AbstractList
+     * @throws NullPointerException if no value is provided or the provided value is null
+     * @return true if this AbstractList has been modified
+     */
+    @Override
+    public boolean add(E e)
+    {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * @param index the location at which to add the specified value
+     * @param element the value to add at the specified index
+     * @throws NullPointerException if no element is provided
+     * @throws IndexOutOfBoundsException if the provided index is out of bounds
+     * @return true if this AbstractList has been modified
+     * @code Inserts the specified element at the specified position in this list.
+     */
+    @Override
+    public void add(int index, E element)
+    {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * @param index the location at which to start adding values into this List
+     * @param c Collection of values to add to this List, starting at the specified index
+     * @throws NullPointerException if no Collection variable is provided, the provided Collection variable is null, or any value IN the provided Collection is null
+     * @throws IndexOutOfBoundsException if the provided index is out of bounds
+     * @return true if this Abstract List has been modified, false otherwise
+     */
+    @Override
+    public boolean addAll(int index, Collection<? extends E> c)
+    {
+        throw new UnsupportedOperationException();
+    }
 }
