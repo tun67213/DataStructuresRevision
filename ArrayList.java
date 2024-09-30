da01789df45d042f85538a067162b2ad1b620d7e
tun67213 <tun67213@temple.edu>
2024-09-30 10:21:35 -0400

    Added the three default constructors

--- ArrayList.java
+++ ArrayList.java
@@ -5,5 +5,31 @@
  */
 public class ArrayList<E> extends AbstractList<E> implements List<E>, RandomAccess, Cloneable, Serializable
 {
-    
+    /**
+     * @code Default constructor for this class
+     */
+    public ArrayList()
+    {
+
+    }
+
+    /**
+     * @param c Collection of values to initialize this ArrayList with
+     * @throws NullPointerException if no Collection variable is provided, the provided Collection variable is null, or any variable in the provided Collection is null
+     * @code This constructor creates a new ArrayList and initializes it with values from this Collection, in the order returned by this Collection's iterator.
+     */
+    public ArrayList(Collection<? extends E> c)
+    {
+
+    }
+
+    /**
+     * @param initialCapacity capacity to initialize this ArrayList with
+     * @throws IllegalArgumentException if the provided index is less than or equal to 0
+     * @code This constructor will initialize this ArrayList with an initialCapacity number of values allocated in the array
+     */
+    public ArrayList(int initialCapacity)
+    {
+        
+    }
 }
