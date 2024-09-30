5fdaf6e3c02a70cd2b39178043120657451477df
tun67213 <tun67213@temple.edu>
2024-09-30 13:05:24 -0400

    Included add(E e)

--- Queue.java
+++ Queue.java
@@ -5,5 +5,12 @@
  */
 public interface Queue<E>
 {
-
+    /**
+     * @param e value to add into this Queue
+     * @throws NullPointerException if no value is provided or the provided value is 'null'
+     * @throws IllegalStateException if no space is available in this Queue
+     * @return true if the provided value was added successfully
+     */
+    @Override
+    boolean add(E e);
 }
