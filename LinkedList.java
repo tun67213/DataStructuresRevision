7108e7a80c889a0ab993e656f35d2a79d77acd17
tun67213 <tun67213@temple.edu>
2024-10-02 18:45:04 -0400

    Implemented poll() methods
    Implemented poll(), pollFirst(), and pollLast().

--- LinkedList.java
+++ LinkedList.java
@@ -1065,7 +1065,23 @@ public class LinkedList<E> extends AbstractSequentialList<E> implements List<E>,
     @Override
     public E poll()
     {
-        throw new UnsupportedOperationException("Not supported yet.");
+        if(head == null)
+        {
+            return null;
+        }
+        Node<E> removingNode = head;
+        head = head.next;
+        if(head == null)
+        {
+            tail = null;
+        }
+        else
+        {
+            head.previous = null;
+            removingNode.next = null;
+        }
+        this.size--;
+        return removingNode.data;
     }
 
     /**
@@ -1075,7 +1091,23 @@ public class LinkedList<E> extends AbstractSequentialList<E> implements List<E>,
     @Override
     public E pollFirst()
     {
-        throw new UnsupportedOperationException("Not supported yet.");
+        if(head == null)
+        {
+            return null;
+        }
+        Node<E> removingNode = head;
+        head = head.next;
+        if(head == null)
+        {
+            tail = null;
+        }
+        else
+        {
+            head.previous = null;
+            removingNode.next = null;
+        }
+        this.size--;
+        return removingNode.data;
     }
 
     /**
@@ -1085,7 +1117,18 @@ public class LinkedList<E> extends AbstractSequentialList<E> implements List<E>,
     @Override
     public E pollLast()
     {
-        throw new UnsupportedOperationException("Not supported yet.");
+        if(tail == null)
+        {
+            return null;
+        }
+        Node<E> removingNode = tail;
+        tail = tail.previous;
+        if(tail == null)
+        {
+            head = null;
+        }
+        this.size--;
+        return removingNode.data;
     }
 
     /**
