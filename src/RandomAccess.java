e79b3cba03db1a051efd565e5e7c0fb9eecab392
tun67213 <tun67213@temple.edu>
2024-10-05 17:54:14 -0400

    Added link back to README

--- src/RandomAccess.java
+++ src/RandomAccess.java
@@ -1,6 +1,8 @@
 package src;
 
-// To return back to the README.md for this file, click here: https://github.com/tun67213/DataStructuresRevision/blob/main/README.md
+// To return back to the README.md for this file:
+//      If on a web browser, simply click here: https://github.com/tun67213/DataStructuresRevision?tab=readme-ov-file#randomaccessjava-serializablejava-cloneablejava
+//      Otherwise, hold command on the Mac keyboard (control if on a Windows) and then click the link above
 /**
  * @author arvindhvelrajan
  * @code Merely an empty interface
--- src/Serializable.java
+++ src/Serializable.java
@@ -1,6 +1,6 @@
 package src;
 
-// To return back to the README.md for this file, click here: https://github.com/tun67213/DataStructuresRevision/blob/main/README.md
+// To return back to the README.md for this file, click here: https://github.com/tun67213/DataStructuresRevision?tab=readme-ov-file#randomaccessjava-serializablejava-cloneablejava
 /**
  * @author arvindhvelrajan
  * @code Merely an empty interface
