64acd91cca2808e8ee542d1e07e2c16c3f5bed06
tun67213 <tun67213@temple.edu>
2024-10-10 16:40:19 -0400

    Created class with main() function

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,16 @@
 package src;
 
-public class Main {
+/**
+ * @author arvindhvelrajan
+ * @code This class is for users to be able to test the existing data structures, specifically ArrayList.java and LinkedList.java, in conjunction with Collection.java.
+ */
+public class Main
+{
+    /**
+     * @param args command line arguments
+     */
+    public static void main(String[] args)
+    {
+
+    }
 }
