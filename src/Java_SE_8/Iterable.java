90406449aca6a08247cbfd5b25141c6ae1432412
tun67213 <tun67213@temple.edu>
2025-01-07 12:48:53 +0900

    Made interface generic

--- src/Java_SE_8/Iterable.java
+++ src/Java_SE_8/Iterable.java
@@ -2,7 +2,10 @@ package src.Java_SE_8;
 
 /**
  * @author arvindhvelrajan
+ * @param <T> Generic variable for this class
  * @code Class that implements an iterable object
  */
-public interface Iterable {
+public interface Iterable<T>
+{
+	
 }
