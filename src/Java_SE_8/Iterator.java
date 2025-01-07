c200173d057e0353036413a1bbc6bbe1825374f7
tun67213 <tun67213@temple.edu>
2025-01-07 12:45:06 +0900

    Made interface generic

--- src/Java_SE_8/Iterator.java
+++ src/Java_SE_8/Iterator.java
@@ -2,7 +2,10 @@ package src.Java_SE_8;
 
 /**
  * @author arvindhvelrajan
+ * @param <E> Generic variable for this interface
  * @code Class that implements an iterator
  */
-public interface Iterator {
+public interface Iterator<E>
+{
+	
 }
