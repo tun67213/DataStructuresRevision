414f4682917fbac3525b940e24100551aebddf1e
tun67213 <tun67213@temple.edu>
2025-01-07 12:47:48 +0900

    Added all methods to be implemented

--- src/Java_SE_8/Iterator.java
+++ src/Java_SE_8/Iterator.java
@@ -1,5 +1,7 @@
 package src.Java_SE_8;
 
+import java.util.function.Consumer;
+
 /**
  * @author arvindhvelrajan
  * @param <E> Generic variable for this interface
@@ -7,5 +9,30 @@ package src.Java_SE_8;
  */
 public interface Iterator<E>
 {
-	
+	/**
+	 * @code Performs the given action for each remaining element until all elements have been processed or the action
+	 * throws an exception.
+	 */
+	default void forEachRemaining(Consumer<? super E> action)
+	{
+
+	}
+
+	/**
+	 * @return true if the iteration has more elements, false otherwise
+	 */
+	boolean hasNext();
+
+	/**
+	 * @return the next element in the iteration
+	 */
+	E next();
+
+	/**
+	 * @code Removes from the underlying collection the last element returned by this iterator.
+	 */
+	default void remove()
+	{
+		throw new UnsupportedOperationException();
+	}
 }
