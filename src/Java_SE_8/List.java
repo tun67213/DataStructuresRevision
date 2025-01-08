f99e4b05a781b10bad564ca529636b6d633b72f4
tun67213 <tun67213@temple.edu>
2025-01-08 20:19:43 +0900

    Made interface generic

--- src/Java_SE_8/List.java
+++ src/Java_SE_8/List.java
@@ -2,7 +2,10 @@ package src.Java_SE_8;
 
 /**
  * @author arvindhvelrajan
+ * @param <E> Generic variable for this interface
  * @code Interface that implements a List
  */
-public interface List {
+public interface List<E>
+{
+	
 }
