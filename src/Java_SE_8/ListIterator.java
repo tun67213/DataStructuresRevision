8f511da9ef428dcf51bef9be8e1e600b7723bde4
tun67213 <tun67213@temple.edu>
2025-01-08 20:19:02 +0900

    Added all methods

--- src/Java_SE_8/ListIterator.java
+++ src/Java_SE_8/ListIterator.java
@@ -7,5 +7,57 @@ package src.Java_SE_8;
  */
 public interface ListIterator<E> extends Iterator<E>
 {
+	/**
+	 * @param e the value to be inserted into this list
+	 * @throws NullPointerException if no value is provided
+	 * @code Inserts the specified element into the list.
+	 */
+	void add(E e);
 
+	/**
+	 * @return true if this list iterator has more elements when traversing the list in the forward direction, false otherwise
+	 */
+	@Override
+	boolean hasNext();
+
+	/**
+	 * @return true if this list iterator has more elements when traversing the list in the reverse direction, false otherwise
+	 */
+	boolean hasPrevious();
+
+	/**
+	 * @return the next element in this list
+	 * @code Returns the next element in this list and advances the cursor position.
+	 */
+	@Override
+	E next();
+
+	/**
+	 * @return  the index of the element that would be returned by a subsequent call to next()
+	 */
+	int nextIndex();
+
+	/**
+	 * @return the previous element in this list
+	 * @code Returns the previous element in this list and moves the cursor backwards.
+	 */
+	E previous();
+
+	/**
+	 * @return the index of the element that would be returned by a subsequent call to previous()
+	 */
+	int previousIndex();
+
+	/**
+	 * @code Removes from the list the last element that was returned by next() or previous().
+	 */
+	@Override
+	void remove();
+
+	/**
+	 * @param e the value to replace the existing one with
+	 * @throws NullPointerException if no value is provided
+	 * @code Replaces the last element returned by next() or previous() with the specified element.
+	 */
+	void set(E e);
 }
