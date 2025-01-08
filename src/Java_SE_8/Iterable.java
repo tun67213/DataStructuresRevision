c8aa9880fa109c16ea02fc22b4735c2cad41dd1e
tun67213 <tun67213@temple.edu>
2025-01-08 09:45:56 +0900

    Added all methods to be implemented

--- src/Java_SE_8/Iterable.java
+++ src/Java_SE_8/Iterable.java
@@ -1,5 +1,8 @@
 package src.Java_SE_8;
 
+import java.util.Spliterator;
+import java.util.function.Consumer;
+
 /**
  * @author arvindhvelrajan
  * @param <T> Generic variable for this class
@@ -7,5 +10,25 @@ package src.Java_SE_8;
  */
 public interface Iterable<T>
 {
-	
+	/**
+	 * Performs the given action for each element of the Iterable until all elements have been processed
+	 * or the action throws an exception.
+	 */
+	default void forEach(Consumer<? super T> action)
+	{
+
+	}
+
+	/**
+	 * @return an iterator over elements of type T
+	 */
+	Iterator<T> iterator();
+
+	/**
+	 * @return a Spliterator over the elements described by this Iterable
+	 */
+	default Spliterator<T> spliterator()
+	{
+		return null;
+	}
 }
