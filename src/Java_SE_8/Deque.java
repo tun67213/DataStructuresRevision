ee0264649ca33925141cabdb33a8c26a68f8389e
tun67213 <tun67213@temple.edu>
2025-01-08 19:36:05 +0900

    Imported interface Queue

--- src/Java_SE_8/Deque.java
+++ src/Java_SE_8/Deque.java
@@ -5,7 +5,7 @@ package src.Java_SE_8;
  * @param <E> Generic variable for this interface
  * @code Class that implements the Deque interface
  */
-public interface Deque<E>
+public interface Deque<E> extends Queue<E>
 {
-	
+
 }
