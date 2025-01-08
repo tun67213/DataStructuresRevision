1d0ff059567e3764792d4d028c5508f88235bc2c
tun67213 <tun67213@temple.edu>
2025-01-08 10:06:35 +0900

    Set up generics and imported module Collection

--- src/Java_SE_8/Queue.java
+++ src/Java_SE_8/Queue.java
@@ -2,7 +2,10 @@ package src.Java_SE_8;
 
 /**
  * @author arvindhvelrajan
+ * @param <E> Generic variable for this class
  * @code Interface that implements a Queue
  */
-public interface Queue {
+public interface Queue<E> extends Collection<E>
+{
+
 }
