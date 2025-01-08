460768157596878304dcd9af08a0fab539f3986d
tun67213 <tun67213@temple.edu>
2025-01-08 19:35:50 +0900

    Made class generic

--- src/Java_SE_8/Deque.java
+++ src/Java_SE_8/Deque.java
@@ -2,7 +2,10 @@ package src.Java_SE_8;
 
 /**
  * @author arvindhvelrajan
+ * @param <E> Generic variable for this interface
  * @code Class that implements the Deque interface
  */
-public interface Deque {
+public interface Deque<E>
+{
+	
 }
