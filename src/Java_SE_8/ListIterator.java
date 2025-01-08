c03f2bd7bf22fb4df74dbd3f176f7e7dafb5f752
tun67213 <tun67213@temple.edu>
2025-01-08 20:13:49 +0900

    Imported interface Iterator

--- src/Java_SE_8/ListIterator.java
+++ src/Java_SE_8/ListIterator.java
@@ -5,7 +5,7 @@ package src.Java_SE_8;
  * @param <E> Generic variable for this interface
  * @code Class that implements an ListIterator object
  */
-public interface ListIterator<E>
+public interface ListIterator<E> extends Iterator<E>
 {
 
 }
