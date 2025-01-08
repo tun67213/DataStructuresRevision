f6ccae0a969bfc65f89346c62663b8e0c28a7d40
tun67213 <tun67213@temple.edu>
2025-01-08 09:48:02 +0900

    Made class generic

--- src/Java_SE_8/Collection.java
+++ src/Java_SE_8/Collection.java
@@ -2,7 +2,10 @@ package src.Java_SE_8;
 
 /**
  * @author arvindhvelrajan
+ * @param <E> Generic variable for this interface
  * @code Interface that implements a Collection
  */
-public interface Collection {
+public interface Collection<E>
+{
+
 }
