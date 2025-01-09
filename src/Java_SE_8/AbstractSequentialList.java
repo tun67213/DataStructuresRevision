91a3384d4ad0581e1afe045a8c989d45cd7d5883
Arvindh Velrajan <tun67213@temple.edu>
2025-01-09 12:04:48 +0900

    Made class generic

--- src/Java_SE_8/AbstractSequentialList.java
+++ src/Java_SE_8/AbstractSequentialList.java
@@ -2,7 +2,10 @@ package src.Java_SE_8;
 
 /**
  * @author arvindhvelrajan
+ * @param <E> Generic variable for this class
  * @code Interface that implements an Abstract Sequential List
  */
-public abstract class AbstractSequentialList {
+public abstract class AbstractSequentialList<E>
+{
+	
 }
