0ed48baf63b0ee33b6fac5fd0f9344fc4f650287
Arvindh Velrajan <tun67213@temple.edu>
2025-01-09 13:10:33 +0900

    Made class generic

--- src/Java_SE_8/LinkedList.java
+++ src/Java_SE_8/LinkedList.java
@@ -2,9 +2,10 @@ package src.Java_SE_8;
 
 /**
  * @author arvindhvelrajan
+ * @param <E> Generic variable for this class
  * @code This class implements LinkedList for Java SE 8
  */
-public class LinkedList
+public class LinkedList<E>
 {
 
 }
