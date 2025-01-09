ecacc87abb9b9976cadb8b7ddf1172de3b0cef77
Arvindh Velrajan <tun67213@temple.edu>
2025-01-09 13:11:28 +0900

    Imported classes associated into this class

--- src/Java_SE_8/LinkedList.java
+++ src/Java_SE_8/LinkedList.java
@@ -5,7 +5,7 @@ package src.Java_SE_8;
  * @param <E> Generic variable for this class
  * @code This class implements LinkedList for Java SE 8
  */
-public class LinkedList<E>
+public class LinkedList<E> extends AbstractSequentialList<E> implements List<E>, Deque<E>, Cloneable, Serializable
 {
 
 }
