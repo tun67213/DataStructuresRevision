d78fda9e2b697c4bea12dc29eb44f4cce66efeee
Arvindh Velrajan <tun67213@temple.edu>
2025-01-09 12:16:24 +0900

    Imported all necessary modules

--- src/Java_SE_8/ArrayList.java
+++ src/Java_SE_8/ArrayList.java
@@ -5,7 +5,7 @@ package src.Java_SE_8;
  * @param <E> Generic variable for this class
  * @code This class implements ArrayList for Java SE 8
  */
-public class ArrayList<E>
+public class ArrayList<E> extends AbstractList<E> implements List<E>, RandomAccess, Cloneable, Serializable
 {
 
 }
