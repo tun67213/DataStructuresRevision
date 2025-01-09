2d3ef6574bbd1be0c160ba8c9301f679ad4b21f7
Arvindh Velrajan <tun67213@temple.edu>
2025-01-09 13:58:54 +0900

    Added all methods that need to be implemented

--- src/Java_SE_8/LinkedList.java
+++ src/Java_SE_8/LinkedList.java
@@ -1,5 +1,8 @@
 package src.Java_SE_8;
 
+import java.util.NoSuchElementException;
+import java.util.Spliterator;
+
 /**
  * @author arvindhvelrajan
  * @param <E> Generic variable for this class
@@ -7,5 +10,503 @@ package src.Java_SE_8;
  */
 public class LinkedList<E> extends AbstractSequentialList<E> implements List<E>, Deque<E>, Cloneable, Serializable
 {
+	/**
+	 * @code Constructs an empty list.
+	 */
+	public LinkedList()
+	{
 
+	}
+
+	/**
+	 * @param c Collection of values to initialize this LinkedList with
+	 * @throws NullPointerException if no Collection is provided
+	 * @code Constructs a list containing the elements of the specified collection, in the order that they are returned by the collection's iterator.
+	 */
+	public LinkedList(Collection<? extends E> c)
+	{
+
+	}
+
+	/**
+	 * @param e the value to be added to the end of this LinkedList
+	 * @throws NullPointerException if no value is provided or the provided value is null
+	 * @return true if the value has successfully been added to the end of this List
+	 * @code Appends the specified element to the end of this list.
+	 */
+	@Override
+	public boolean add(E e)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
+	 * @param index the location at which to add the specified value to this list
+	 * @param element the value to be added to the specified location in this list
+	 * @throws NullPointerException if no element is provided or the provided element is null
+	 * @throws IndexOutOfBoundsException if the provided index is out of bounds
+	 * @code Inserts the specified element at the specified position in this list.
+	 */
+	@Override
+	public void add(int index, E element)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
+	 * @param c Collection of values to add to the end of this list
+	 * @throws NullPointerException if the provided Collection is null or any value in the provided Collection is null
+	 * @return true if all the values have been successfully added, false if the provided Collection is empty
+	 * @code Appends all of the elements in the specified collection to the end of this list, in the order that they are returned by the specified collection's iterator.
+	 */
+	@Override
+	public boolean addAll(Collection<? extends E> c)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
+	 * @param index the location to start adding values from
+	 * @param c Collection of values to add to this List, starting at the specified index
+	 * @throws NullPointerException if the Collection variable is null or any value in the Collection is null
+	 * @throws IndexOutOfBoundsException if the provided index is out of bounds
+	 * @return true if all the values have been successfully added, false if the provided Collection is empty
+	 * @code Inserts all of the elements in the specified collection into this list, starting at the specified position.
+	 */
+	@Override
+	public boolean addAll(int index, Collection<? extends E> c)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
+	 * @param e the value to add to the beginning of this LinkedList
+	 * @throws NullPointerException if no value is provided
+	 * @code Inserts the specified element at the beginning of this list.
+	 */
+	@Override
+	public void addFirst(E e)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
+	 * @param e the value to add to the end of this LinkedList
+	 * @throws NullPointerException if no value is provided
+	 * @code Inserts the specified element to the end of this list.
+	 */
+	@Override
+	public void addLast(E e)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
+	 * @code Removes all the elements from this list.
+	 */
+	@Override
+	public void clear()
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
+	 * @return a shallow copy of this LinkedList
+	 */
+	@Override
+	public Object clone()
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
+	 * @param o the Object to find the existence of in this LinkedList
+	 * @throws NullPointerException if no Object is provided
+	 * @return true if the Object is found, false otherwise
+	 */
+	@Override
+	public boolean contains(Object o)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
+	 * @param c Collection of values to find the existence of in this LinkedList
+	 * @throws NullPointerException if no Collection variable is provided or the provided variable is null
+	 * @return false if a value is not found or null, true if all values are found
+	 */
+	@Override
+	public boolean containsAll(Collection<?> c)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
+	 * @return an iterator over the elements in this deque in reverse sequential order
+	 */
+	@Override
+	public Iterator<E> descendingIterator()
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
+	 * @return the head (first element) of this list
+	 * @code Retrieves, but does not remove, the head (first element) of this list.
+	 */
+	@Override
+	public E element()
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
+	 * @param index the location at which to retrieve a value from
+	 * @throws IndexOutOfBoundsException if the provided index is out of bounds
+	 * @return the value at the specified index in this List
+	 */
+	@Override
+	public E get(int index)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
+	 * @return the first element in this list
+	 */
+	@Override
+	public E getFirst()
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
+	 * @return the last element in this list
+	 */
+	@Override
+	public E getLast()
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
+	 * @return the hash code value for this list
+	 */
+	@Override
+	public int hashCode()
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
+	 * @param o the Object to find the first occurrence of
+	 * @throws NullPointerException if no Object is provided
+	 * @return the location of the first occurrence of the provided Object or -1 if not found
+	 */
+	@Override
+	public int indexOf(Object o)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
+	 * @return an iterator over the elements in this LinkedList, in sequential order
+	 */
+	@Override
+	public Iterator<E> iterator()
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
+	 * @return true if this list contains no elements, false otherwise
+	 */
+	@Override
+	public boolean isEmpty()
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
+	 * @param o the Object to find the last occurrence of
+	 * @throws NullPointerException if no Object is provided or the provided Object is 'null'
+	 * @return the location of the last occurrence of the specified Object or -1 if not found
+	 */
+	@Override
+	public int lastIndexOf(Object o)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
+	 * @return a list iterator over the elements in this list (in proper sequence)
+	 */
+	@Override
+	public ListIterator<E> listIterator()
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
+	 * @param index the location at which to start the list
+	 * @return a list iterator over the elements in this list (in proper sequence), starting at the specified position in this list
+	 */
+	@Override
+	public ListIterator<E> listIterator(int index)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
+	 * @param e the value to add to the end of this list
+	 * @throws NullPointerException if no value is provided
+	 * @return true if the provided value was added successfully
+	 * @code Adds the specified element as the tail (last element) of this list.
+	 */
+	@Override
+	public boolean offer(E e)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
+	 * @param e the value to add to the beginning of this list
+	 * @throws NullPointerException if no value is provided
+	 * @return true if the provided value was added successfully
+	 * @code Inserts the specified element at the front of this list.
+	 */
+	@Override
+	public boolean offerFirst(E e)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
+	 * @param e the value to add to the end of this list
+	 * @throws NullPointerException if no value is provided
+	 * @return true if the provided value was added successfully
+	 * @code Inserts the specified element at the end of this list.
+	 */
+	@Override
+	public boolean offerLast(E e)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
+	 * @return the head (first element) of this list
+	 * @code Retrieves, but does not remove, the head (first element) of this list.
+	 */
+	@Override
+	public E peek()
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
+	 * @return the head (first element) of this list or null
+	 * @code Retrieves, but does not remove, the first element of this list, or returns null if this list is empty.
+	 */
+	@Override
+	public E peekFirst()
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
+	 * @return the tail (last element) of this list or null
+	 * @code Retrieves, but does not remove, the last element of this list, or returns null if this list is empty.
+	 */
+	@Override
+	public E peekLast()
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
+	 * @throws NoSuchElementException if this list is empty
+	 * @return the value in the node previously defined as head
+	 */
+	@Override
+	public E poll()
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
+	 * @return the value in the node previously defined as head, or null if this list is empty
+	 */
+	@Override
+	public E pollFirst()
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
+	 * @return the value in the node previously defined as tail, or null if this list is empty
+	 */
+	@Override
+	public E pollLast()
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
+	 * @return the value previously at the top of the stack represented by this LinkedList
+	 */
+	@Override
+	public E pop()
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
+	 * @param e the value to be added to the top of the stack represented by this LinkedList
+	 * @throws NullPointerException if no value is provided
+	 * @code Pushes an element onto the stack represented by this list.
+	 */
+	@Override
+	public void push(E e)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
+	 * @return the value in the node previously defined as head before removal
+	 * @throws NoSuchElementException if this LinkedList is empty
+	 */
+	@Override
+	public E remove()
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
+	 * @param index the location at which to remove a value from
+	 * @throws IndexOutOfBoundsException if the provided index is out of bounds
+	 * @return the value previously at the specified index in this LinkedList
+	 * @code Removes the element at the specified position in this list.
+	 */
+	@Override
+	public E remove(int index)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
+	 * @param o the Object to find and remove the first occurrence of
+	 * @throws NullPointerException if no Object is provided
+	 * @return true if this List has been modified, false otherwise
+	 * @code Removes the first occurrence of the specified element from this list, if it is present.
+	 */
+	@Override
+	public boolean remove(Object o)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
+	 * @return the value previously the first value in this list
+	 * @code Removes and returns the first element from this list.
+	 */
+	@Override
+	public E removeFirst()
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
+	 * @param o the Object to find and remove the first occurrence of
+	 * @throws NullPointerException if no Object is provided or the provided Object is null
+	 * @return true if this LinkedList has been modified, false otherwise
+	 * @code Removes the first occurrence of the specified element in this list (when traversing the list from head to tail).
+	 */
+	@Override
+	public boolean removeFirstOccurrence(Object o)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
+	 * @return the value in the Node previously at tail
+	 * @code Removes and returns the last element from this list.
+	 */
+	@Override
+	public E removeLast()
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
+	 * @param o the Object to find and remove the last occurrence of
+	 * @throws NullPointerException if no Object is provided or the provided Object is null
+	 * @return true if this LinkedList has been modified, false otherwise
+	 * @code Removes the last occurrence of the specified element in this list (when traversing the list from head to tail).
+	 */
+	@Override
+	public boolean removeLastOccurrence(Object o)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
+	 * @param index the location at which to replace a value
+	 * @param element the replacement value
+	 * @throws NullPointerException if no replacement value is provided or the provided replacement value is 'null'
+	 * @throws IndexOutOfBoundsException if the provided index is out of bounds
+	 * @return the value previously in the Node at the specified index
+	 * @code Replaces the element at the specified position in this list with the specified element.
+	 */
+	@Override
+	public E set(int index, E element)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
+	 * @return the number of elements in this list
+	 */
+	@Override
+	public int size()
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
+	 * @return a late-binding and fail-fast Spliterator over the elements in this list
+	 */
+	@Override
+	public Spliterator<E> spliterator()
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
+	 * @return an array containing all of the elements in this list in proper sequence (from first to last element)
+	 */
+	@Override
+	public Object[] toArray()
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
+	 * @param a an array of type T into which to copy all the values from this LinkedList into
+	 * @throws NullPointerException if no array is provided
+	 * @return the provided array with values from this LinkedList copied into it
+	 */
+	@Override
+	public <T> T[] toArray(T[] a)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
+	 * @return a String representation of this LinkedList
+	 */
+	@Override
+	public String toString()
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
 }
