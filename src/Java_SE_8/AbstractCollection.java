8bfbd35c9394799ea44d1a7494b8953820a4da22
tun67213 <tun67213@temple.edu>
2025-01-09 10:02:39 +0900

    Made class generic

--- src/Java_SE_8/AbstractCollection.java
+++ src/Java_SE_8/AbstractCollection.java
@@ -2,9 +2,10 @@ package src.Java_SE_8;
 
 /**
  * @author arvindhvelrajan
+ * @param <E> Generic variable for this class
  * @code Interface that implements an Abstract Collection
  */
-public abstract class AbstractCollection
+public abstract class AbstractCollection<E>
 {
 
 }
