58be4d7e51f7bfd44e2ae241e325302f96e5646f
Arvindh Velrajan <tun67213@temple.edu>
2025-01-09 12:15:31 +0900

    Made class generic

--- src/Java_SE_8/ArrayList.java
+++ src/Java_SE_8/ArrayList.java
@@ -2,9 +2,10 @@ package src.Java_SE_8;
 
 /**
  * @author arvindhvelrajan
+ * @param <E> Generic variable for this class
  * @code This class implements ArrayList for Java SE 8
  */
-public class ArrayList
+public class ArrayList<E>
 {
 
 }
