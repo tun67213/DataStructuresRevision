bcb522bd7728c008aa48608925aaab3ba69de3d3
Arvindh Velrajan <tun67213@temple.edu>
2025-01-09 11:40:51 +0900

    Made class generic

--- src/Java_SE_8/AbstractList.java
+++ src/Java_SE_8/AbstractList.java
@@ -2,7 +2,10 @@ package src.Java_SE_8;
 
 /**
  * @author arvindhvelrajan
+ * @param <E> Generic variable for this class
  * @code Interface that implements an Abstract List
  */
-public abstract class AbstractList {
+public abstract class AbstractList<E>
+{
+
 }
