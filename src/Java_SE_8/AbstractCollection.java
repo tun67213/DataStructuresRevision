c5dba9e3380788ed1694806b7df13c3e9f46c977
tun67213 <tun67213@temple.edu>
2025-01-09 10:02:54 +0900

    Imported methods from Collection

--- src/Java_SE_8/AbstractCollection.java
+++ src/Java_SE_8/AbstractCollection.java
@@ -5,7 +5,7 @@ package src.Java_SE_8;
  * @param <E> Generic variable for this class
  * @code Interface that implements an Abstract Collection
  */
-public abstract class AbstractCollection<E>
+public abstract class AbstractCollection<E> implements Collection<E>
 {
 
 }
