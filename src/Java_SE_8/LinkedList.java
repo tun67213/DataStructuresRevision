50caa29d94edbe8b807de6d91a71f6ea1af2ad08
Arvindh Velrajan <tun67213@temple.edu>
2025-01-10 09:59:22 +0900

    Implemented both contains() methods

--- src/Java_SE_8/LinkedList.java
+++ src/Java_SE_8/LinkedList.java
@@ -377,7 +377,18 @@ public class LinkedList<E> extends AbstractSequentialList<E> implements List<E>,
 	@Override
 	public boolean contains(Object o)
 	{
-		throw new UnsupportedOperationException("Not supported yet.");
+		if(o == null)
+		{
+			return false;
+		}
+		Node<E> current = head;
+		// Iterate through this list with the condition that the value in the Node isn't what we are searching for
+		while(current != null && !(current.data.equals(o)))
+		{
+			current = current.next;
+		}
+		// If current isn't null, that means that the data we are looking for has been found
+		return current != null;
 	}
 
 	/**
@@ -388,7 +399,33 @@ public class LinkedList<E> extends AbstractSequentialList<E> implements List<E>,
 	@Override
 	public boolean containsAll(Collection<?> c)
 	{
-		throw new UnsupportedOperationException("Not supported yet.");
+		if(c == null)
+		{
+			throw new NullPointerException("You MUST provide a collection of values to find the existence of in this LinkedList");
+		}
+		if(c.isEmpty())
+		{
+			return true;
+		}
+		Iterator<?> iterator = c.iterator();
+		while(iterator.hasNext())
+		{
+			Object value = iterator.next();
+			if(value == null)
+			{
+				return false;
+			}
+			Node<E> current = head;
+			while(current != null && !(current.data.equals(value)))
+			{
+				current = current.next;
+			}
+			if(current == null)
+			{
+				return false;
+			}
+		}
+		return true;
 	}
 
 	/**
