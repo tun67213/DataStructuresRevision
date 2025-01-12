240a64ed751e216279aa063324fd98ba712fe718
Arvindh Velrajan <tun67213@temple.edu>
2025-01-12 13:32:15 -0500

    Added a main file for users to test with

---
+++ src/Java_SE_8/Main.java
@@ -0,0 +1,20 @@
+package src.Java_SE_8;
+
+/**
+ * @author arvindhvelrajan
+ * @code This is the Main function to test any classes that are in Java SE 8
+ */
+public class Main
+{
+	/**
+	 * @param args command line arguments
+	 */
+	public static void main(String[] args)
+	{
+		// To test a method here, simply do the following steps:
+		// 1) Write the class name with the data type (i.e. ArrayList<Integer> list = new ArrayList<>();)
+
+		// Once that is done, write 'list.' with the '.' included to see a list of all the available methods.
+
+	}
+}
