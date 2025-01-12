afdd42bd8dae09535c3fc3e52a8a5b104b5afb99
Arvindh Velrajan <tun67213@temple.edu>
2025-01-11 22:29:40 -0500

    Implemented peek(), peekFirst(), and peekLast()

--- src/Java_SE_8/LinkedList.java
+++ src/Java_SE_8/LinkedList.java
@@ -1095,7 +1095,11 @@ public class LinkedList<E> extends AbstractSequentialList<E> implements List<E>,
 	@Override
 	public E peek()
 	{
-		throw new UnsupportedOperationException("Not supported yet.");
+		if(head == null)
+		{
+			return null;
+		}
+		return head.data;
 	}
 
 	/**
@@ -1105,7 +1109,11 @@ public class LinkedList<E> extends AbstractSequentialList<E> implements List<E>,
 	@Override
 	public E peekFirst()
 	{
-		throw new UnsupportedOperationException("Not supported yet.");
+		if(head == null)
+		{
+			return null;
+		}
+		return head.data;
 	}
 
 	/**
@@ -1115,7 +1123,11 @@ public class LinkedList<E> extends AbstractSequentialList<E> implements List<E>,
 	@Override
 	public E peekLast()
 	{
-		throw new UnsupportedOperationException("Not supported yet.");
+		if(tail == null)
+		{
+			return null;
+		}
+		return tail.data;
 	}
 
 	/**
