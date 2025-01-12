73ef24acacf8323367f05a65601f0425d1b7aa59
Arvindh Velrajan <tun67213@temple.edu>
2025-01-12 11:18:30 -0500

    Implemented method set()

--- src/Java_SE_8/LinkedList.java
+++ src/Java_SE_8/LinkedList.java
@@ -1546,7 +1546,22 @@ public class LinkedList<E> extends AbstractSequentialList<E> implements List<E>,
 	@Override
 	public E set(int index, E element)
 	{
-		throw new UnsupportedOperationException("Not supported yet.");
+		if(element == null)
+		{
+			throw new NullPointerException("You MUST provide a non-null Object to set the element to the LinkedList");
+		}
+		if(index < 0 || index >= this.size)
+		{
+			throw new IndexOutOfBoundsException("Index " + index + " is out of bounds");
+		}
+		Node<E> current = head;
+		for(int i = 0; i < index; i++)
+		{
+			current = current.next;
+		}
+		E value = current.data;
+		current.data = element;
+		return value;
 	}
 
 	/**
