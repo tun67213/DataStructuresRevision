30ea11d4e3db0d28c00eefcadec8603fae3e405e
Arvindh Velrajan <tun67213@temple.edu>
2025-01-14 21:14:57 -0500

    Extended interface Iterator<E>

--- src/Java_SE_9/ListIterator.java
+++ src/Java_SE_9/ListIterator.java
@@ -5,7 +5,7 @@ package src.Java_SE_9;
  * @param <E> Generic variable for this interface
  * @code This interface implements the ListIterator Object for Java SE 9.
  */
-public interface ListIterator<E>
+public interface ListIterator<E> extends Iterator<E>
 {
-	
+
 }
