bc4fa1ee78e2f282e0051a635ad5f93c5cbed6e3
Arvindh Velrajan <tun67213@temple.edu>
2025-01-14 21:09:00 -0500

    Made interface generic

--- src/Java_SE_9/Iterable.java
+++ src/Java_SE_9/Iterable.java
@@ -2,7 +2,10 @@ package src.Java_SE_9;
 
 /**
  * @author arvindhvelrajan
+ * @param <T> Generic variable for this interface
  * @code This interface defines the Iterable Object for Java SE 9.
  */
-public interface Iterable {
+public interface Iterable<T>
+{
+
 }
