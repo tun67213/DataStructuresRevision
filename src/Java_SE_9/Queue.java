9cbba431c35539e9f52da67ec646fa10738ebf9c
Arvindh Velrajan <tun67213@temple.edu>
2025-01-14 22:24:25 -0500

    Added all methods to be implemented later

--- src/Java_SE_9/Queue.java
+++ src/Java_SE_9/Queue.java
@@ -7,5 +7,44 @@ package src.Java_SE_9;
  */
 public interface Queue<E> extends Collection<E>
 {
-	
+	/**
+	 * @param e the value to be added to this queue
+	 * @throws NullPointerException if no value is provided
+	 * @throws IllegalStateException if no space is available
+	 * @return true if the provided value was added successfully
+	 */
+	@Override
+	boolean add(E e);
+
+	/**
+	 * @return the value at the head of this queue
+	 * @code Retrieves, but does not remove, the head of this queue.
+	 */
+	E element();
+
+	/**
+	 * @param e the value to offer to this Queue
+	 * @throws NullPointerException if no value is provided
+	 * @return true if the value was added successfully
+	 * @code Inserts the specified element into this queue if it is possible to do so immediately without violating capacity restrictions.
+	 */
+	boolean offer(E e);
+
+	/**
+	 * @return the head of this queue or null if this queue is empty
+	 * @code Retrieves, but does not remove, the head of this queue, or returns null if this queue is empty.
+	 */
+	E peek();
+
+	/**
+	 * @return the value previously at head, or null if head is null
+	 * @code Retrieves and removes the head of this queue, or returns null if this queue is empty.
+	 */
+	E poll();
+
+	/**
+	 * @return the value at the node previously the head of this queue
+	 * @code Retrieves and removes the head of this queue.
+	 */
+	E remove();
 }
