de371c4ae28176c697215366039fc2ed2ca4a37d
Arvindh Velrajan <tun67213@temple.edu>
2025-01-15 09:30:12 -0500

    Added variables

--- src/Java_SE_9/AbstractList.java
+++ src/Java_SE_9/AbstractList.java
@@ -6,5 +6,6 @@ package src.Java_SE_9;
  */
 public abstract class AbstractList<E> extends AbstractCollection<E> implements List<E>
 {
-
+	// This variable keeps track of the number of times this list has been structurally modified
+	protected int modCount;
 }
