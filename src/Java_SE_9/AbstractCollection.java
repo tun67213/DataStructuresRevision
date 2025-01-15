5ad2c90d1c54b84939b4b9a22b0e303117dcd110
Arvindh Velrajan <tun67213@temple.edu>
2025-01-15 09:16:59 -0500

    Imported methods from Collection

--- src/Java_SE_9/AbstractCollection.java
+++ src/Java_SE_9/AbstractCollection.java
@@ -4,7 +4,7 @@ package src.Java_SE_9;
  * @param <E> Generic variable for this class
  * @code This class implements the methods involved with AbstractCollection for Java SE 9
  */
-public abstract class AbstractCollection<E>
+public abstract class AbstractCollection<E> implements Collection<E>
 {
 	
 }
