91c9793ddcb1f1c00f6dd34216b2fb3d74759091
Arvindh Velrajan <tun67213@temple.edu>
2025-01-15 09:29:15 -0500

    Imported AbstractCollection<E> and List<E>

--- src/Java_SE_9/AbstractList.java
+++ src/Java_SE_9/AbstractList.java
@@ -4,7 +4,7 @@ package src.Java_SE_9;
  * @param <E> Generic variable for this class
  * @code This class implements the methods involved with AbstractList for Java SE 9
  */
-public abstract class AbstractList<E>
+public abstract class AbstractList<E> extends AbstractCollection<E> implements List<E>
 {
-	
+
 }
