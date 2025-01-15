d3ca8baed280c60bcd74967551b3b0b9ed64d754
Arvindh Velrajan <tun67213@temple.edu>
2025-01-14 22:25:01 -0500

    Made interface generic

--- src/Java_SE_9/Deque.java
+++ src/Java_SE_9/Deque.java
@@ -2,7 +2,10 @@ package src.Java_SE_9;
 
 /**
  * @author arvindhvelrajan
+ * @param <E> Generic variable for this interface
  * @code This interface implements the ListIterator Object for Java SE 9.
  */
-public interface Deque {
+public interface Deque<E>
+{
+	
 }
