4b83be9c8f28b167702229f8f3c3ac6b46417ea0
Arvindh Velrajan <tun67213@temple.edu>
2025-01-14 21:05:09 -0500

    Made interface generic

--- src/Java_SE_9/Iterator.java
+++ src/Java_SE_9/Iterator.java
@@ -2,7 +2,10 @@ package src.Java_SE_9;
 
 /**
  * @author arvindhvelrajan
+ * @param <E> Generic variable for this interface
  * @code This interface defines the Iterator Object.
  */
-public interface Iterator {
+public interface Iterator<E>
+{
+	
 }
