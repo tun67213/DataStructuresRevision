ec01c0d7349099d5defa0310acfdf549dd0fb28d
Arvindh Velrajan <tun67213@temple.edu>
2025-01-15 09:28:50 -0500

    Added generics to class

--- src/Java_SE_9/AbstractList.java
+++ src/Java_SE_9/AbstractList.java
@@ -1,7 +1,10 @@
 package src.Java_SE_9;
 
 /**
+ * @param <E> Generic variable for this class
  * @code This class implements the methods involved with AbstractList for Java SE 9
  */
-public abstract class AbstractList {
+public abstract class AbstractList<E>
+{
+	
 }
