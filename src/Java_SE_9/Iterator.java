9e9ee2cae285827bac2a7d45764befb65395ed25
Arvindh Velrajan <tun67213@temple.edu>
2025-01-14 21:08:22 -0500

    Added methods for iterator and imported Consumer

--- src/Java_SE_9/Iterator.java
+++ src/Java_SE_9/Iterator.java
@@ -1,5 +1,7 @@
 package src.Java_SE_9;
 
+import java.util.function.Consumer;
+
 /**
  * @author arvindhvelrajan
  * @param <E> Generic variable for this interface
@@ -7,5 +9,31 @@ package src.Java_SE_9;
  */
 public interface Iterator<E>
 {
-	
+	/**
+	 * @param action the action to be performed for each of the remaining values until all values have been processed
+	 * @throws NullPointerException if no action is provided
+	 * @code Performs the given action for each remaining element until all elements have been processed or the action throws an exception.
+	 */
+	default void forEachRemaining(Consumer<? super E> action)
+	{
+
+	}
+
+	/**
+	 * @return true if the iteration has more elements
+	 */
+	boolean hasNext();
+
+	/**
+	 * @return the next element in the iteration
+	 */
+	E next();
+
+	/**
+	 * @code Removes from the underlying collection the last element returned by this iterator.
+	 */
+	default void remove()
+	{
+
+	}
 }
