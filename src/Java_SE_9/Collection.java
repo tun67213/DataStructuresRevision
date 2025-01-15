2699b9ea2d63dfd1602deb8f8d48ccf6d57c107a
Arvindh Velrajan <tun67213@temple.edu>
2025-01-14 21:47:25 -0500

    Made interface generic

--- src/Java_SE_9/Collection.java
+++ src/Java_SE_9/Collection.java
@@ -2,7 +2,10 @@ package src.Java_SE_9;
 
 /**
  * @author arvindhvelrajan
+ * @param <E> Generic variable for this interface
  * @code This interface implements the Collection Object for Java SE 9.
  */
-public interface Collection {
+public interface Collection<E>
+{
+
 }
