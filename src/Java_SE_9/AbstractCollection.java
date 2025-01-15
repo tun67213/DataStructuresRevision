ca7fd975a9e5f4a8c838b883081419e7b27ff8f9
Arvindh Velrajan <tun67213@temple.edu>
2025-01-15 09:16:38 -0500

    Made class generic

--- src/Java_SE_9/AbstractCollection.java
+++ src/Java_SE_9/AbstractCollection.java
@@ -1,7 +1,10 @@
 package src.Java_SE_9;
 
 /**
+ * @param <E> Generic variable for this class
  * @code This class implements the methods involved with AbstractCollection for Java SE 9
  */
-public abstract class AbstractCollection {
+public abstract class AbstractCollection<E>
+{
+	
 }
