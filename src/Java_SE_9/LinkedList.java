d5c6f5371a4c727f742bf007fe794d965791b588
Arvindh Velrajan <tun67213@temple.edu>
2025-01-16 09:31:40 -0500

    Implemented method get()

--- src/Java_SE_9/LinkedList.java
+++ src/Java_SE_9/LinkedList.java
@@ -465,7 +465,16 @@ public class LinkedList<E> extends AbstractSequentialList<E> implements List<E>,
 	@Override
 	public E get(int index)
 	{
-		throw new UnsupportedOperationException("Not supported yet.");
+		if(index < 0 || index >= this.size)
+		{
+			throw new IndexOutOfBoundsException("Index " + index + " is out of bounds");
+		}
+		Node<E> current = head;
+		for(int i = 0; i < index; i++)
+		{
+			current = current.next;
+		}
+		return current.data;
 	}
 
 	/**
