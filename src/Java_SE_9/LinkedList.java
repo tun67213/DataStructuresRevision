f7940298b36ad87bf9df435d6251b2662255b468
Arvindh Velrajan <tun67213@temple.edu>
2025-01-16 09:41:51 -0500

    Implemented method isEmpty()

--- src/Java_SE_9/LinkedList.java
+++ src/Java_SE_9/LinkedList.java
@@ -621,7 +621,7 @@ public class LinkedList<E> extends AbstractSequentialList<E> implements List<E>,
 	@Override
 	public boolean isEmpty()
 	{
-		throw new UnsupportedOperationException("Not supported yet.");
+		return this.size == 0;
 	}
 
 	/**
