cad7f7007c2b27ad555e2370b2e32c98929812e6
Arvindh Velrajan <tun67213@temple.edu>
2025-01-15 21:32:27 -0500

    Imported module Spliterator() for spliterator()

--- src/Java_SE_9/ArrayList.java
+++ src/Java_SE_9/ArrayList.java
@@ -4,6 +4,7 @@ import java.util.function.Consumer;
 import java.util.function.Predicate;
 import java.util.function.UnaryOperator;
 import java.util.Comparator;
+import java.util.Spliterator;
 
 /**
  * @author arvindhvelrajan
