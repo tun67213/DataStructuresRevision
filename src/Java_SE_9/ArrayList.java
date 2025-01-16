d7287df87868ff8f55cac6c8edad0579f416ab2d
Arvindh Velrajan <tun67213@temple.edu>
2025-01-15 19:41:10 -0500

    Imported modules to be implemented

--- src/Java_SE_9/ArrayList.java
+++ src/Java_SE_9/ArrayList.java
@@ -5,7 +5,7 @@ package src.Java_SE_9;
  * @param <E> Generic variable fo this class
  * @code This class implements the methods associated with ArrayList for Java SE 9.
  */
-public class ArrayList<E>
+public class ArrayList<E> extends AbstractList<E> implements List<E>, RandomAccess, Cloneable, Serializable
 {
 	
 }
