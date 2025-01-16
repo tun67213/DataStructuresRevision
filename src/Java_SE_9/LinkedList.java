44b48f8879640b72635dfa04a2de672b6e98b067
Arvindh Velrajan <tun67213@temple.edu>
2025-01-15 21:41:11 -0500

    Imported modules necessary to be added to this class

--- src/Java_SE_9/LinkedList.java
+++ src/Java_SE_9/LinkedList.java
@@ -5,7 +5,7 @@ package src.Java_SE_9;
  * @param <E> Generic variable for this class
  * @code This class implements the methods associated with LinkedList for Java SE 9.
  */
-public class LinkedList<E>
+public class LinkedList<E> extends AbstractSequentialList<E> implements List<E>, Deque<E>, Cloneable, Serializable
 {
 
 }
