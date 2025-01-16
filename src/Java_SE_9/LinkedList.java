5f595a17067c178fca43e952a3c986ff74287d90
Arvindh Velrajan <tun67213@temple.edu>
2025-01-16 08:20:21 -0500

    Implemented constructors

--- src/Java_SE_9/LinkedList.java
+++ src/Java_SE_9/LinkedList.java
@@ -37,7 +37,9 @@ public class LinkedList<E> extends AbstractSequentialList<E> implements List<E>,
 	 */
 	public LinkedList()
 	{
-
+		this.size = 0;
+		head = null;
+		tail = null;
 	}
 
 	/**
@@ -47,7 +49,33 @@ public class LinkedList<E> extends AbstractSequentialList<E> implements List<E>,
 	 */
 	public LinkedList(Collection<? extends E> c)
 	{
-
+		if(c == null || c.contains(null))
+		{
+			throw new NullPointerException("You MUST provide a non-null collection of non-null elements to initialize this LinkedList with");
+		}
+		this.size = 0;
+		this.head = null;
+		this.tail = null;
+		if(!c.isEmpty())
+		{
+			Iterator<? extends E> iterator = c.iterator();
+			while(iterator.hasNext())
+			{
+				Node<E> newNode = new Node<>(iterator.next());
+				if(head == null)
+				{
+					head = newNode;
+					tail = newNode;
+				}
+				else
+				{
+					tail.next = newNode;
+					newNode.previous = tail;
+					tail = tail.next;
+				}
+				this.size++;
+			}
+		}
 	}
 
 	/**
