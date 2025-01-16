7873732a92d7bc493aafdcdfe93ca4f8885f72ca
Arvindh Velrajan <tun67213@temple.edu>
2025-01-16 08:38:13 -0500

    Implemented method clear()

--- src/Java_SE_9/LinkedList.java
+++ src/Java_SE_9/LinkedList.java
@@ -335,7 +335,9 @@ public class LinkedList<E> extends AbstractSequentialList<E> implements List<E>,
 	@Override
 	public void clear()
 	{
-		throw new UnsupportedOperationException("Not supported yet.");
+		this.size = 0;
+		head = null;
+		tail = null;
 	}
 
 	/**
