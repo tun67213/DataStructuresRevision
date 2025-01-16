561a2734f4e7afcaba52ba8fae08b42186ac960e
Arvindh Velrajan <tun67213@temple.edu>
2025-01-15 21:02:52 -0500

    Added method indexOf()

--- src/Java_SE_9/ArrayList.java
+++ src/Java_SE_9/ArrayList.java
@@ -156,4 +156,14 @@ public class ArrayList<E> extends AbstractList<E> implements List<E>, RandomAcce
 	{
 		throw new UnsupportedOperationException("Not supported yet.");
 	}
+
+	/**
+	 * @param o the Object to find the location of the first occurrence of
+	 * @return the location of the first occurrence of the specified Object or -1 if the provided Object is null or not found
+	 */
+	@Override
+	public int indexOf(Object o)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
 }
