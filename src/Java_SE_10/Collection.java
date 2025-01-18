e35efedf7cf7f1039fd82baaf0451ba710ae0639
Arvindh Velrajan <tun67213@temple.edu>
2025-01-17 20:53:30 -0500

    Made interface generic

--- src/Java_SE_10/Collection.java
+++ src/Java_SE_10/Collection.java
@@ -2,8 +2,9 @@ package src.Java_SE_10;
 
 /**
  * @author arvindhvelrajan
+ * @param <E> Generic variable for this interface
  */
-public interface Collection
+public interface Collection<E>
 {
 	
 }
