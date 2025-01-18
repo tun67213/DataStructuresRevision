0bb6f85ba9afd493e1114db149902b900eefb2e9
Arvindh Velrajan <tun67213@temple.edu>
2025-01-18 10:38:49 -0500

    Imported interfaces that need to be implemented

--- src/Java_SE_10/ArrayList.java
+++ src/Java_SE_10/ArrayList.java
@@ -4,7 +4,7 @@ package src.Java_SE_10;
  * @author arvindhvelrajan
  * @param <E> Generic method for this class
  */
-public class ArrayList<E>
+public class ArrayList<E> extends AbstractList<E> implements List<E>, RandomAccess, Cloneable, Serializable
 {
-	
+
 }
