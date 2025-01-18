6e7b6dd8725277cafac5c6cb6df06dfad4ce6372
Arvindh Velrajan <tun67213@temple.edu>
2025-01-17 19:49:38 -0500

    Moved open brace to a new line

--- src/Java_SE_10/ListIterator.java
+++ src/Java_SE_10/ListIterator.java
@@ -4,5 +4,7 @@ package src.Java_SE_10;
  * @param <E> Generic variable for this interface
  * @code This interface implements the ListIterator for Java SE 10
  */
-public interface ListIterator<E> {
+public interface ListIterator<E>
+{
+	
 }
