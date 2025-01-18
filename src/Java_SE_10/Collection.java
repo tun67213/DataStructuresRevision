f3717ce1bbc27bc6f56eb6adbf0601d23e786526
Arvindh Velrajan <tun67213@temple.edu>
2025-01-17 20:53:50 -0500

    Extended interface Iterable<E>

--- src/Java_SE_10/Collection.java
+++ src/Java_SE_10/Collection.java
@@ -4,7 +4,7 @@ package src.Java_SE_10;
  * @author arvindhvelrajan
  * @param <E> Generic variable for this interface
  */
-public interface Collection<E>
+public interface Collection<E> extends Iterable<E>
 {
-	
+
 }
