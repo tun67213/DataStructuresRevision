de247aa9065ba815980dfd1dd5beccf9a1f8e0e5
Arvindh Velrajan <tun67213@temple.edu>
2025-01-18 22:27:49 -0500

    Imported methods to be implemented

--- src/Java_SE_10/LinkedList.java
+++ src/Java_SE_10/LinkedList.java
@@ -4,7 +4,7 @@ package src.Java_SE_10;
  * @author arvindhvelrajan
  * @param <E> Generic variable for this class
  */
-public class LinkedList<E>
+public class LinkedList<E> extends AbstractSequentialList<E> implements List<E>, Deque<E>, Cloneable, Serializable
 {
 
 }
