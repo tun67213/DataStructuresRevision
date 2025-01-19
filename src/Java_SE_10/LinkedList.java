59a573280b2067e18711cc9b491b42cc7bd0b370
Arvindh Velrajan <tun67213@temple.edu>
2025-01-18 22:29:15 -0500

    Added constructors to be implemented

--- src/Java_SE_10/LinkedList.java
+++ src/Java_SE_10/LinkedList.java
@@ -6,5 +6,21 @@ package src.Java_SE_10;
  */
 public class LinkedList<E> extends AbstractSequentialList<E> implements List<E>, Deque<E>, Cloneable, Serializable
 {
+	/**
+	 * @code Constructs an empty list.
+	 */
+	public LinkedList()
+	{
 
+	}
+
+	/**
+	 * @param c Collection of values to initialize this LinkedList with
+	 * @throws NullPointerException if the provided variable is null or the provided Collection contains null values
+	 * @code Constructs a list containing the elements of the specified collection, in the order they are returned by the collection's iterator.
+	 */
+	public LinkedList(Collection<? extends E> c)
+	{
+		
+	}
 }
