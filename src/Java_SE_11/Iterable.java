61af9c49a83091fa19da5508559c1d89c48cf0c3
Arvindh Velrajan <tun67213@temple.edu>
2025-01-19 18:20:43 -0500

    Added all methods

--- src/Java_SE_11/Iterable.java
+++ src/Java_SE_11/Iterable.java
@@ -6,5 +6,26 @@ package src.Java_SE_11;
  */
 public interface Iterable<T>
 {
+	/**
+	 * @param action the action to be performed on each element in this Iterable
+	 * @throws NullPointerException if no action is provided
+	 * @code Performs the given action for each element of the Iterable until all elements have been processed or the action throws an exception.
+	 */
+	default void forEach(Consumer<? super T> action)
+	{
+		throw new UnsupportedOperationException("To be implemented in classes that implement it");
+	}
 
+	/**
+	 * @return an iterator over the elements of type T
+	 */
+	Iterator<T> iterator();
+
+	/**
+	 * @return a Spliterator over the elements described by this Iterable
+	 */
+	default Spliterator<T> spliterator()
+	{
+		throw new UnsupportedOperationException("To be implemented in classes that implement it");
+	}
 }
