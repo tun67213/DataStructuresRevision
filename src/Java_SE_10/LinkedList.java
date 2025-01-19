cb26613263872c30d30bd91de18067e26c43eff6
Arvindh Velrajan <tun67213@temple.edu>
2025-01-19 11:32:58 -0500

    Implemented method size()

--- src/Java_SE_10/LinkedList.java
+++ src/Java_SE_10/LinkedList.java
@@ -1519,7 +1519,7 @@ public class LinkedList<E> extends AbstractSequentialList<E> implements List<E>,
 	@Override
 	public int size()
 	{
-		throw new UnsupportedOperationException("Not supported yet.");
+		return this.size;
 	}
 
 	/**
