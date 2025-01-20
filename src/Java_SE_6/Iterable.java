eaa73af491bbee79b98b18d48c4fe19042dc4827
Arvindh Velrajan <tun67213@temple.edu>
2025-01-20 17:28:45 -0500

    Changed generic variable from E to T

--- src/Java_SE_6/Iterable.java
+++ src/Java_SE_6/Iterable.java
@@ -2,10 +2,10 @@ package src.Java_SE_6;
 
 /**
  * @author arvindhvelrajan
- * @param <E> Generic variable for this interface
+ * @param <T> Generic variable for this interface
  * @code Interface that implements Iterable for Java SE 6
  */
-public interface Iterable<E>
+public interface Iterable<T>
 {
 
 }
