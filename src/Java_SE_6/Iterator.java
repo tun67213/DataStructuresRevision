21dca47096b729d8e7cc00f574a19f1955838b95
Arvindh Velrajan <tun67213@temple.edu>
2025-01-20 17:30:48 -0500

    Added all methods for interface

--- src/Java_SE_6/Iterator.java
+++ src/Java_SE_6/Iterator.java
@@ -7,5 +7,18 @@ package src.Java_SE_6;
  */
 public interface Iterator<E>
 {
+	/**
+	 * @return true if the iteration has more elements, false otherwise
+	 */
+	boolean hasNext();
 
+	/**
+	 * @return the next element in the iteration
+	 */
+	E next();
+
+	/**
+	 * @code Removes from the underlying collection the last element returned by the iterator.
+	 */
+	void remove();
 }
