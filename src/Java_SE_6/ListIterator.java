e408ae60b937f9bd5dce4c546917b53a6748a15a
Arvindh Velrajan <tun67213@temple.edu>
2025-01-20 17:36:29 -0500

    Added all methods

--- src/Java_SE_6/ListIterator.java
+++ src/Java_SE_6/ListIterator.java
@@ -7,5 +7,55 @@ package src.Java_SE_6;
  */
 public interface ListIterator<E> extends Iterator<E>
 {
+	/**
+	 * @param e the value to be added to this list
+	 * @throws NullPointerException if no value is provided
+	 * @code Inserts the specified element into the list.
+	 */
+	void add(E e);
 
+	/**
+	 * @return true if this list iterator has more elements when traversing the list in the forward direction, false otherwise
+	 */
+	@Override
+	boolean hasNext();
+
+	/**
+	 * @return true if this list iterator has more elements when traversing the list in the reverse direction, false otherwise
+	 */
+	boolean hasPrevious();
+
+	/**
+	 * @return the next element in the list
+	 */
+	@Override
+	E next();
+
+	/**
+	 * @return the index of the element that would be returned by a subsequent call to next()
+	 */
+	int nextIndex();
+
+	/**
+	 * @return the previous element in the list
+	 */
+	E previous();
+
+	/**
+	 * @return the index of the element that would be returned by a subsequent call to previous()
+	 */
+	int previousIndex();
+
+	/**
+	 * @code Removes from the list the last element that was returned by next() or previous()
+	 */
+	@Override
+	void remove();
+
+	/**
+	 * @param e the replacement value for the specified position in this list
+	 * @throws NullPointerException if no value is provided
+	 * @code Replaces the last element returned by next or previous with the specified element.
+	 */
+	void set(E e);
 }
