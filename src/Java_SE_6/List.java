cbe3262a7612461290a4b3d41f0a9ba8add5b94f
Arvindh Velrajan <tun67213@temple.edu>
2025-01-20 16:42:15 -0500

    Added JavaDoc to interface

--- src/Java_SE_6/List.java
+++ src/Java_SE_6/List.java
@@ -1,4 +1,8 @@
 package src.Java_SE_6;
 
+/**
+ * @author arvindhvelrajan
+ * @code Interface that implements List for Java SE 6
+ */
 public interface List {
 }
--- src/Java_SE_6/RandomAccess.java
+++ src/Java_SE_6/RandomAccess.java
@@ -1,4 +1,8 @@
 package src.Java_SE_6;
 
+/**
+ * @author arvindhvelrajan
+ * @code Merely an empty interface
+ */
 public interface RandomAccess {
 }
