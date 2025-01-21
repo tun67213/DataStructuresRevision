b742fa1da534d77918fe49736f26e277f939c43a
Arvindh Velrajan <tun67213@temple.edu>
2025-01-20 20:50:39 -0500

    Made class generic

--- src/Java_SE_6/AbstractList.java
+++ src/Java_SE_6/AbstractList.java
@@ -2,8 +2,9 @@ package src.Java_SE_6;
 
 /**
  * @author arvindhvelrajan
+ * @param <E> Generic variable for this class
  */
-public abstract class AbstractList
+public abstract class AbstractList<E>
 {
 
 }
--- src/Java_SE_6/AbstractSequentialList.java
+++ src/Java_SE_6/AbstractSequentialList.java
@@ -2,8 +2,9 @@ package src.Java_SE_6;
 
 /**
  * @author arvindhvelrajan
+ * @param <E> Generic variable for this class
  */
-public abstract class AbstractSequentialList
+public abstract class AbstractSequentialList<E>
 {
 
 }
