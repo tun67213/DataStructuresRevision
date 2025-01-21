e2cf27af6a5f07c2135f42281e8a1e5946e42536
Arvindh Velrajan <tun67213@temple.edu>
2025-01-21 08:32:53 -0500

    Imported methods from Collection interface

--- src/Java_SE_6/Queue.java
+++ src/Java_SE_6/Queue.java
@@ -5,7 +5,7 @@ package src.Java_SE_6;
  * @param <E> Generic variable for this interface
  * @code Interface that implements Queue for Java SE 6
  */
-public interface Queue<E>
+public interface Queue<E> extends Collection<E>
 {
 
 }
