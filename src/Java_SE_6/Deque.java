0ffd0749376d70071f00b3516ce45b81a0947784
Arvindh Velrajan <tun67213@temple.edu>
2025-01-21 08:38:52 -0500

    Imported methods from Queue

--- src/Java_SE_6/Deque.java
+++ src/Java_SE_6/Deque.java
@@ -5,7 +5,7 @@ package src.Java_SE_6;
  * @param <E> Generic variable for this interface
  * @code Interface that implements Deque for Java SE 6
  */
-public interface Deque<E>
+public interface Deque<E> extends Queue<E>
 {
 
 }
