7ac3cc31d8223dd664c6a7c7b1fab53f15fd1251
Arvindh Velrajan <tun67213@temple.edu>
2025-01-22 10:12:47 -0500

    Importing methods from Collection<E>

--- src/Java_SE_6/AbstractCollection.java
+++ src/Java_SE_6/AbstractCollection.java
@@ -4,7 +4,7 @@ package src.Java_SE_6;
  * @author arvindhvelrajan
  * @param <E> Generic variable for this class
  */
-public abstract class AbstractCollection<E>
+public abstract class AbstractCollection<E> implements Collection<E>
 {
 
 }
