10cd013bb5d4536a74f86fe2025a52bf4d885673
Arvindh Velrajan <tun67213@temple.edu>
2025-01-23 09:31:23 -0500

    Added all methods
    All methods were added. Any methods here are the user's preference on whether or not to implement.

--- src/Java_SE_6/AbstractList.java
+++ src/Java_SE_6/AbstractList.java
@@ -6,5 +6,192 @@ package src.Java_SE_6;
  */
 public abstract class AbstractList<E> extends AbstractCollection<E> implements List<E>
 {
+	/**
+	 * Counts the number of times this list has been structurally modified.
+	 */
+	protected int modCount;
 
+	/**
+	 * @code Sole constructor.
+	 */
+	protected AbstractList()
+	{
+
+	}
+
+	/**
+	 * @param e the value to be added to the end of this list
+	 * @throws NullPointerException if no value is provided
+	 * @return true if this List has been modified
+	 * @code Appends the specified element to the end of this list.
+	 */
+	@Override
+	public boolean add(E e)
+	{
+		throw new UnsupportedOperationException("Not supported.");
+	}
+
+	/**
+	 * @param index the location at which to add a value
+	 * @param element the value to be added to the specified location in this List
+	 * @throws NullPointerException if no value is provided or the provided value is null
+	 * @throws IndexOutOfBoundsException if the provided index is out of bounds
+	 * @code Inserts the specified element at the specified position in this list.
+	 */
+	@Override
+	public void add(int index, E element)
+	{
+		throw new UnsupportedOperationException("Not supported.");
+	}
+
+	/**
+	 * @param index the location at which to start adding values to this List
+	 * @param c Collection of values to add to this List, starting at the specified index
+	 * @throws NullPointerException if no Collection is provided or the provided Collection contains null values
+	 * @throws IndexOutOfBoundsException if the provided index is out of bounds
+	 * @return true if this List has been modified, false otherwise
+	 */
+	public boolean add(int index, Collection<? extends E> c)
+	{
+		throw new UnsupportedOperationException("Not supported.");
+	}
+
+	/**
+	 * @code Removes all of the elements from this list.
+	 */
+	@Override
+	public void clear()
+	{
+		throw new UnsupportedOperationException("Not supported.");
+	}
+
+	/**
+	 * @param o the Object to compare this list with
+	 * @return false if the provided Object is null or not equal to this List, true otherwise
+	 * @code Compares the specified object with this list for equality.
+	 */
+	@Override
+	public boolean equals(Object o)
+	{
+		throw new UnsupportedOperationException("Not supported.");
+	}
+
+	/**
+	 * @param index the location at which to retrieve a value from
+	 * @throws IndexOutOfBoundsException if the provided index is out of bounds
+	 * @return the value at the specified index in this List
+	 */
+	@Override
+	public abstract E get(int index);
+
+	/**
+	 * @return the hash code value for this list
+	 */
+	@Override
+	public int hashCode()
+	{
+		throw new UnsupportedOperationException("Not supported.");
+	}
+
+	/**
+	 * @param o the Object to find the existence of in this List
+	 * @return -1 if the Object is null or not found, the index of the first occurrence of otherwise
+	 * @code Returns the index of the first occurrence of the specified element in this list, or -1 if this list does not contain the element.
+	 */
+	@Override
+	public int indexOf(Object o)
+	{
+		throw new UnsupportedOperationException("Not supported.");
+	}
+
+	/**
+	 * @return an iterator over the elements in this list in proper sequence
+	 */
+	@Override
+	public Iterator<E> iterator()
+	{
+		throw new UnsupportedOperationException("Not supported.");
+	}
+
+	/**
+	 * @param o the Object to find the last occurrence of in this List
+	 * @return -1 if the Object is null or not found, the index of the last occurrence of otherwise
+	 * @code Returns the index of the last occurrence of the specified element in this list, or -1 if this list does not contain the element.
+	 */
+	@Override
+	public int lastIndexOf(Object o)
+	{
+		throw new UnsupportedOperationException("Not supported.");
+	}
+
+	/**
+	 * @return a list iterator over the elements in this list (in proper sequence)
+	 */
+	@Override
+	public ListIterator<E> listIterator()
+	{
+		throw new UnsupportedOperationException("Not supported.");
+	}
+
+	/**
+	 * @param index the location from which to create this listIterator from
+	 * @throws IndexOutOfBoundsException if the provided index is out of bounds
+	 * @return a list iterator of the elements in this list (in proper sequence), starting at the specified position in this list
+	 */
+	@Override
+	public ListIterator<E> listIterator(int index)
+	{
+		throw new UnsupportedOperationException("Not supported.");
+	}
+
+	/**
+	 * @param index the location from which to remove a value
+	 * @throws IndexOutOfBoundsException if the provided index is out of bounds
+	 * @return the value previously at the specified index
+	 * @code Removes the element at the specified position in this list.
+	 */
+	@Override
+	public E remove(int index)
+	{
+		throw new UnsupportedOperationException("Not supported.");
+	}
+
+	/**
+	 * @param fromIndex the location from which to remove values
+	 * @param toIndex the location until which to remove values
+	 * @throws IndexOutOfBoundsException if either is out of bounds
+	 * @throws IllegalArgumentException if the provided fromIndex is greater than the provided toIndex
+	 * @code Removes from this list all of the elements whose index is between fromIndex, inclusive, and toIndex, exclusive.
+	 */
+	protected void removeRange(int fromIndex, int toIndex)
+	{
+		throw new UnsupportedOperationException("Not supported.");
+	}
+
+	/**
+	 * @param index the location at which to set a value
+	 * @param element the replacement value for the specified index
+	 * @throws NullPointerException if no element is provided or the provided element is null
+	 * @throws IndexOutOfBoundsException if the provided index is out of bounds
+	 * @return the value previously at the specified index before replacement
+	 * @code  Replaces the element at the specified position in this list with the specified element.
+	 */
+	@Override
+	public E set(int index, E element)
+	{
+		throw new UnsupportedOperationException("Not supported.");
+	}
+
+	/**
+	 * @param fromIndex the location from which to create a subList from
+	 * @param toIndex the location until which to create a subList
+	 * @throws IndexOutOfBoundsException if the provided index is out of bounds
+	 * @throws IllegalArgumentException if fromIndex is greater than toIndex
+	 * @return a view of the portion of this list between the specified fromIndex, inclusive, and toIndex, exclusive
+	 */
+	@Override
+	public List<E> subList(int fromIndex, int toIndex)
+	{
+		throw new UnsupportedOperationException("Not supported.");
+	}
 }
