97091c741c07ebccaab95f5690be9703f71a7ef9
Arvindh Velrajan <tun67213@temple.edu>
2025-01-23 09:43:44 -0500

    Imported modules with methods that need to be implemented

--- src/Java_SE_6/ArrayList.java
+++ src/Java_SE_6/ArrayList.java
@@ -4,7 +4,7 @@ package src.Java_SE_6;
  * @author arvindhvelrajan
  * @param <E> Generic variable for this class
  */
-public class ArrayList<E> extends AbstractList<E>
+public class ArrayList<E> extends AbstractList<E> implements List<E>, RandomAccess, Cloneable, Serializable
 {
 
 }
