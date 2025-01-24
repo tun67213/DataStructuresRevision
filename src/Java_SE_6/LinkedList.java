9cec1e289e9075a94e30965b8001f05597c3cecb
Arvindh Velrajan <tun67213@temple.edu>
2025-01-24 08:32:17 -0500

    Implemented both addAll() methods

--- src/Java_SE_6/LinkedList.java
+++ src/Java_SE_6/LinkedList.java
@@ -143,7 +143,33 @@ public class LinkedList<E> extends AbstractSequentialList<E> implements List<E>,
 	@Override
 	public boolean addAll(Collection<? extends E> c)
 	{
-		throw new UnsupportedOperationException("Not supported yet.");
+		if(c == null || c.contains(null))
+		{
+			throw new NullPointerException("You MUST provide a non-null Collection of non-null values to add to this LinkedList");
+		}
+		if(c.isEmpty())
+		{
+			return false;
+		}
+		Iterator<? extends E> iterator = c.iterator();
+		Node<E> newNode;
+		while(iterator.hasNext())
+		{
+			newNode = new Node<>(iterator.next());
+			if(tail == null)
+			{
+				head = newNode;
+				tail = newNode;
+			}
+			else
+			{
+				tail.next = newNode;
+				newNode.previous = tail;
+				tail = tail.next;
+			}
+			this.size++;
+		}
+		return true;
 	}
 
 	/**
@@ -156,7 +182,66 @@ public class LinkedList<E> extends AbstractSequentialList<E> implements List<E>,
 	@Override
 	public boolean addAll(int index, Collection<? extends E> c)
 	{
-		throw new UnsupportedOperationException("Not supported yet.");
+		if(c == null || c.contains(null))
+		{
+			throw new NullPointerException("You MUST provide a non-null Collection of non-null elements to add to this LinkedList");
+		}
+		if(index < 0 || index > this.size)
+		{
+			throw new IndexOutOfBoundsException("Index " + index + " is out of bounds");
+		}
+		E[] values = (E[]) c.toArray();
+		if(head == null || index == this.size)
+		{
+			Iterator<? extends E> iterator = c.iterator();
+			while(iterator.hasNext())
+			{
+				Node<E> newNode = new Node<>(iterator.next());
+				if(head == null)
+				{
+					head = newNode;
+					tail = newNode;
+				}
+				else
+				{
+					tail.next = newNode;
+					newNode.previous = tail;
+					tail = tail.next;
+				}
+				this.size++;
+			}
+		}
+		else if(index == 0)
+		{
+			for(int i = values.length - 1; i >= 0; i--)
+			{
+				Node<E> newNode = new Node<>(values[i]);
+				head.previous = newNode;
+				newNode.next = head;
+				head = head.previous;
+				this.size++;
+			}
+		}
+		else
+		{
+			Node<E> current = head;
+			for(int i = 0; i < index - 1; i++)
+			{
+				current = current.next;
+			}
+			Iterator<? extends E> iterator = c.iterator();
+			while(iterator.hasNext())
+			{
+				Node<E> newNode = new Node<>(iterator.next());
+				newNode.next = current.next;
+				newNode.previous = current;
+				current.next.previous = newNode;
+				current.next = newNode;
+				current = current.next;
+				this.size++;
+			}
+		}
+		return true;
 	}
 
 	/**
