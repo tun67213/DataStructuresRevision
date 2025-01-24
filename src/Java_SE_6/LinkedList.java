fddd6f12335ea2136df382c585f919ba4e8c9023
Arvindh Velrajan <tun67213@temple.edu>
2025-01-24 09:04:29 -0500

    Implemented all get() methods
    Implemented get(), getFirst(), and getLast()

--- src/Java_SE_6/LinkedList.java
+++ src/Java_SE_6/LinkedList.java
@@ -478,7 +478,16 @@ public class LinkedList<E> extends AbstractSequentialList<E> implements List<E>,
 	@Override
 	public E get(int index)
 	{
-		throw new UnsupportedOperationException("Not supported yet.");
+		if(index < 0 || index >= this.size)
+		{
+			throw new IndexOutOfBoundsException("Index " + index + " is out of bounds");
+		}
+		Node<E> current = head;
+		for(int i = 0; i < index; i++)
+		{
+			current = current.next;
+		}
+		return current.data;
 	}
 
 	/**
@@ -488,7 +497,11 @@ public class LinkedList<E> extends AbstractSequentialList<E> implements List<E>,
 	@Override
 	public E getFirst()
 	{
-		throw new UnsupportedOperationException("Not supported yet.");
+		if(head == null)
+		{
+			throw new NoSuchElementException("This LinkedList is empty");
+		}
+		return head.data;
 	}
 
 	/**
@@ -498,7 +511,11 @@ public class LinkedList<E> extends AbstractSequentialList<E> implements List<E>,
 	@Override
 	public E getLast()
 	{
-		throw new UnsupportedOperationException("Not supported yet.");
+		if(tail == null)
+		{
+			throw new NoSuchElementException("This LinkedList is empty");
+		}
+		return tail.data;
 	}
 
 	/**
