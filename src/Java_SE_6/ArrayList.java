6e2e7bb0cbe69fee47987bc6ff9455cc45b0f3f9
Arvindh Velrajan <tun67213@temple.edu>
2025-01-23 21:17:13 -0500

    Implemented method size()

--- src/Java_SE_6/ArrayList.java
+++ src/Java_SE_6/ArrayList.java
@@ -854,7 +854,7 @@ public class ArrayList<E> extends AbstractList<E> implements List<E>, RandomAcce
 	@Override
 	public int size()
 	{
-		throw new UnsupportedOperationException("Not supported yet.");
+		return this.size;
 	}
 
 	/**
