cb2a3d80ad72b90924970ccdaafba97b96f09aa6
Arvindh Velrajan <tun67213@temple.edu>
2025-08-06 17:43:43 -0400

    Updated JavaDoc

--- src/Java_SE_5/ArrayList.java
+++ src/Java_SE_5/ArrayList.java
@@ -2,6 +2,7 @@ package Java_SE_5;
 
 /**
  * @author arvindhvelrajan
+ * @code This interface implements ArrayList for Java SE 5.
  */
 public class ArrayList {
 }
--- src/Java_SE_5/Collection.java
+++ src/Java_SE_5/Collection.java
@@ -2,6 +2,7 @@ package Java_SE_5;
 
 /**
  * @author arvindhvelrajan
+ * @code This interface implements Collection for Java SE 5.
  */
 public interface Collection {
 }
