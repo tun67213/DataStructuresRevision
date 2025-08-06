5df01b6e8407a25375d3560126581f070e3a222a
Arvindh Velrajan <tun67213@temple.edu>
2025-08-06 19:18:10 -0400

    Made interface generic

--- src/Java_SE_5/Iterable.java
+++ src/Java_SE_5/Iterable.java
@@ -2,9 +2,10 @@ package Java_SE_5;
 
 /**
  * @author arvindhvelrajan
+ * @param <T> Generic variable for this interface
  * @code This interface implements Iterable for Java SE 5.
  */
-public interface Iterable
+public interface Iterable<T>
 {
 	
 }
