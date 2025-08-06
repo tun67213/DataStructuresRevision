7267022d9bfa4fbda13287dab3d51dc594cf69ab
Arvindh Velrajan <tun67213@temple.edu>
2025-08-06 19:05:45 -0400

    Made class abstract

--- src/Java_SE_5/AbstractCollection.java
+++ src/Java_SE_5/AbstractCollection.java
@@ -4,5 +4,7 @@ package Java_SE_5;
  * @author arvindhvelrajan
  * @code This interface implements AbstractCollection for Java SE 5.
  */
-public class AbstractCollection {
+public abstract class AbstractCollection
+{
+
 }
