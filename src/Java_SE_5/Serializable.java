6d047acefa6ae01076f28d50700fdc5656c8411f
Arvindh Velrajan <tun67213@temple.edu>
2025-08-06 17:41:09 -0400

    Added implementation note for empty interface

--- src/Java_SE_5/Serializable.java
+++ src/Java_SE_5/Serializable.java
@@ -2,6 +2,7 @@ package Java_SE_5;
 
 /**
  * @author arvindhvelrajan
+ * @implNote This is merely an empty interface, i.e., there are no variables nor methods associated with it.
  */
 public interface Serializable {
 }
