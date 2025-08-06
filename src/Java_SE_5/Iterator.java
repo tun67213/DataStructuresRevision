b129a95ad79538e84e6f730c7921906b1338c9b6
Arvindh Velrajan <tun67213@temple.edu>
2025-08-06 19:09:40 -0400

    Added all methods

--- src/Java_SE_5/Iterator.java
+++ src/Java_SE_5/Iterator.java
@@ -7,5 +7,21 @@ package Java_SE_5;
  */
 public interface Iterator<E>
 {
-	
+	/**
+	 * @return true if the iteration has more elements, false otherwise
+	 * @code Returns true if the iteration has more elements.
+	 */
+	boolean hasNext();
+
+	/**
+	 * @return the next element in the iteration
+	 * @throws NoSuchElementException if this iterator has reached the end of this list
+	 * @code Returns the next element in the iteration.
+	 */
+	E next();
+
+	/**
+	 * @code Removes from the underlying collection the last element returned by the iterator.
+	 */
+	void remove();
 }
