6320b23f3ef72b5efffa266367f66824a51015e2
Arvindh Velrajan <tun67213@temple.edu>
2025-08-06 19:22:11 -0400

    Made interface generic

--- src/Java_SE_5/Collection.java
+++ src/Java_SE_5/Collection.java
@@ -2,9 +2,10 @@ package Java_SE_5;
 
 /**
  * @author arvindhvelrajan
+ * @param <E> Generic variable for this interface
  * @code This interface implements Collection for Java SE 5.
  */
-public interface Collection
+public interface Collection<E>
 {
 	
 }
