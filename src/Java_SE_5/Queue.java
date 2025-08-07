8244cbe8d52dd51c168a9a3aabd16e2291d60a4f
Arvindh Velrajan <tun67213@temple.edu>
2025-08-07 13:08:29 -0400

    Made interface generic

--- src/Java_SE_5/Queue.java
+++ src/Java_SE_5/Queue.java
@@ -2,9 +2,10 @@ package Java_SE_5;
 
 /**
  * @author arvindhvelrajan
+ * @param <E> Generic variable for this interface
  * @code This interface implements Queue for Java SE 5.
  */
-public interface Queue
+public interface Queue<E>
 {
 	
 }
