9d029db18f91bdf7da5203882a78d31bd920ec65
Arvindh Velrajan <tun67213@temple.edu>
2025-08-07 13:00:05 -0400

    Formatted bracket

--- src/Java_SE_5/ListIterator.java
+++ src/Java_SE_5/ListIterator.java
@@ -4,5 +4,7 @@ package Java_SE_5;
  * @author arvindhvelrajan
  * @code This interface implements ListIterator for Java SE 5.
  */
-public interface ListIterator {
+public interface ListIterator
+{
+	
 }
