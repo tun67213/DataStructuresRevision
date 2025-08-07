fac0e0cdfc40cab1f2048ed170215d002f0aefd2
Arvindh Velrajan <tun67213@temple.edu>
2025-08-07 18:04:42 -0400

    Made class generic

--- src/Java_SE_5/AbstractCollection.java
+++ src/Java_SE_5/AbstractCollection.java
@@ -2,9 +2,10 @@ package Java_SE_5;
 
 /**
  * @author arvindhvelrajan
+ * @param <E> Generic variable for this class
  * @code This interface implements AbstractCollection for Java SE 5.
  */
-public abstract class AbstractCollection
+public abstract class AbstractCollection<E>
 {
 
 }
