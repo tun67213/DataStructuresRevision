88f57355370f929ec86713784ac6e47b6382f426
Arvindh Velrajan <tun67213@temple.edu>
2025-08-07 18:05:58 -0400

    Added implementation note

--- src/Java_SE_5/AbstractCollection.java
+++ src/Java_SE_5/AbstractCollection.java
@@ -4,6 +4,7 @@ package Java_SE_5;
  * @author arvindhvelrajan
  * @param <E> Generic variable for this class
  * @code This interface implements AbstractCollection for Java SE 5.
+ * @implNote None of the methods here will be implemented. Implement at own will.
  */
 public abstract class AbstractCollection<E> implements Collection<E>
 {
--- src/Java_SE_5/AbstractList.java
+++ src/Java_SE_5/AbstractList.java
@@ -3,6 +3,7 @@ package Java_SE_5;
 /**
  * @author arvindhvelrajan
  * @code This interface implements AbstractList for Java SE 5.
+ * @implNote None of the methods here will be implemented. Implement at own will.
  */
 public abstract class AbstractList
 {
