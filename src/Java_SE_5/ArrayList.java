d9f01dcecdba631600cc16226a9de0428940165a
Arvindh Velrajan <tun67213@temple.edu>
2025-08-07 22:00:07 -0400

    Made class generic

--- src/Java_SE_5/ArrayList.java
+++ src/Java_SE_5/ArrayList.java
@@ -2,9 +2,10 @@ package Java_SE_5;
 
 /**
  * @author arvindhvelrajan
+ * @param <E> Generic variable for this class
  * @code This interface implements ArrayList for Java SE 5.
  */
-public class ArrayList
+public class ArrayList<E>
 {
 	
 }
