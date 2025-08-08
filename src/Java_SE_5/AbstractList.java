a09a3b0b62225de31a726599542b3c5268225a0e
Arvindh Velrajan <tun67213@temple.edu>
2025-08-07 21:21:21 -0400

    Made class generic

--- src/Java_SE_5/AbstractList.java
+++ src/Java_SE_5/AbstractList.java
@@ -2,10 +2,11 @@ package Java_SE_5;
 
 /**
  * @author arvindhvelrajan
+ * @param <E> Generic variable for this class
  * @code This interface implements AbstractList for Java SE 5.
  * @implNote None of the methods here will be implemented. Implement at own will.
  */
-public abstract class AbstractList
+public abstract class AbstractList<E>
 {
 
 }
