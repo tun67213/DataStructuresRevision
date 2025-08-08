d1a29b310251713c04d911a4109fba38b464e50b
Arvindh Velrajan <tun67213@temple.edu>
2025-08-07 22:01:19 -0400

    Imported necessary modules

--- src/Java_SE_5/ArrayList.java
+++ src/Java_SE_5/ArrayList.java
@@ -5,7 +5,7 @@ package Java_SE_5;
  * @param <E> Generic variable for this class
  * @code This interface implements ArrayList for Java SE 5.
  */
-public class ArrayList<E>
+public class ArrayList<E> extends AbstractList<E> implements List<E>, RandomAccess, Cloneable, Serializable
 {
 	
 }
