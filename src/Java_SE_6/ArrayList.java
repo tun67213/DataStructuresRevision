29f2827461e0bd060e7fd6224582d21bc693981b
Arvindh Velrajan <tun67213@temple.edu>
2025-08-10 11:05:24 -0400

    Brought open bracket to new line for formatting

--- src/Java_SE_6/ArrayList.java
+++ src/Java_SE_6/ArrayList.java
@@ -4,5 +4,7 @@ package Java_SE_6;
  * @author arvindhvelrajan
  * @code This class adds all methods associated with ArrayList for Java SE 6.
  */
-public class ArrayList {
+public class ArrayList
+{
+
 }
--- src/Java_SE_6/List.java
+++ src/Java_SE_6/List.java
@@ -4,5 +4,7 @@ package Java_SE_6;
  * @author arvindhvelrajan
  * @code This interface adds all methods associated with List for Java SE 6.
  */
-public interface List {
+public interface List
+{
+
 }
