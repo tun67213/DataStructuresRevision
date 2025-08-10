b5981f0b121e213df60fd782288ce80fb45a9e9e
Arvindh Velrajan <tun67213@temple.edu>
2025-08-10 19:02:54 -0400

    Extended interface Collection<E>

--- src/Java_SE_6/List.java
+++ src/Java_SE_6/List.java
@@ -5,7 +5,7 @@ package Java_SE_6;
  * @param <E> Generic variable for this interface
  * @code This interface adds all methods associated with List for Java SE 6.
  */
-public interface List<E>
+public interface List<E> extends Collection<E>
 {
 
 }
