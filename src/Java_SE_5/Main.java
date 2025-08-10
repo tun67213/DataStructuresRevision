a0ecc8b5cacfa6eec0ef82b3ed366b966a5b3425
Arvindh Velrajan <tun67213@temple.edu>
2025-08-10 11:03:26 -0400

    Set up class for testing methods

--- src/Java_SE_5/Main.java
+++ src/Java_SE_5/Main.java
@@ -2,6 +2,15 @@ package Java_SE_5;
 
 /**
  * @author arvindhvelrajan
+ * @code This file is meant for testing out any methods associated with Java SE 5.
  */
-public class Main {
+public class Main
+{
+	/**
+	 * @param args command line arguments
+	 */
+	public static void main(String[] args)
+	{
+
+	}
 }
