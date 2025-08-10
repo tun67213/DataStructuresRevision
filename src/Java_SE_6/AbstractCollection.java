889805c1eb93d1c28a7d2488a6926f6f9ea97f4d
Arvindh Velrajan <tun67213@temple.edu>
2025-08-10 10:57:47 -0400

    Updated JavaDoc to explain file's reason

--- src/Java_SE_6/AbstractCollection.java
+++ src/Java_SE_6/AbstractCollection.java
@@ -2,6 +2,7 @@ package Java_SE_6;
 
 /**
  * @author arvindhvelrajan
+ * @code This class adds all methods associated with Abstract Collection for Java SE 6.
  */
 public abstract class AbstractCollection {
 }
