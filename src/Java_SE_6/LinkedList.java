9cfa8059f723848f3f878f8e5e4893aa28656c0e
Arvindh Velrajan <tun67213@temple.edu>
2025-08-11 23:59:09 -0400

    Implemented both toArray() methods
    Implemented Object[] toArray() and <T> T[] toArray(T[] a).

--- src/Java_SE_6/LinkedList.java
+++ src/Java_SE_6/LinkedList.java
@@ -1873,7 +1873,14 @@ public class LinkedList<E> extends AbstractSequentialList<E> implements List<E>,
 	@Override
 	public Object[] toArray()
 	{
-		throw new UnsupportedOperationException("Not implemented yet.");
+		E[] values = (E[]) new Object[this.size];
+		int currentSize = 0;
+		for(Node<E> current = head; current != null; current = current.next)
+		{
+			values[currentSize] = current.data;
+			currentSize++;
+		}
+		return values;
 	}
 
 	/**
@@ -1886,7 +1893,28 @@ public class LinkedList<E> extends AbstractSequentialList<E> implements List<E>,
 	@Override
 	public <T> T[] toArray(T[] a)
 	{
-		throw new UnsupportedOperationException("Not implemented yet.");
+		if(a == null)
+		{
+			throw new NullPointerException("You MUST provide a non-null array to copy values from this ArrayList into");
+		}
+		if(a.length < this.size)
+		{
+			a = (T[]) java.lang.reflect.Array.newInstance(a.getClass().getComponentType(), this.size);
+		}
+		else
+		{
+			int currentSize = 0;
+			for(Node<E> current = head; current != null; current = current.next)
+			{
+				a[currentSize] = (T) current.data;
+				currentSize++;
+			}
+			if(a.length > this.size)
+			{
+				a[this.size] = null;
+			}
+		}
+		return a;
 	}
 
 	/**
