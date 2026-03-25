07215bd033b66a80cf89904cee2b1a924dbbef49
Arvindh Velrajan <tun67213@temple.edu>
2026-03-25 19:21:17 +0900

    Add comments and make class generic
    Add comments describing what the whole code is going to be about. Then, also declare the generic variable, and make the class generic.

--- src/Java_SE_7/AbstractCollection.java
+++ src/Java_SE_7/AbstractCollection.java
@@ -1,5 +1,11 @@
 package Java_SE_7;
 
-public class AbstractCollection
+/**
+ * @author arvindhvelrajan
+ * @param <E> Generic variable for this class
+ * @code This class adds all methods associated with Abstract Collection for Java SE 7.
+ * @implNote None of the methods in this class will be implemented. Feel free to implement it at your own interest.
+ */
+public class AbstractCollection<E>
 {
 }
