4d36fbae69b1b865226e4dab90301405497df802
Arvindh Velrajan <tun67213@temple.edu>
2026-03-26 19:42:20 +0900

    Add JavaDoc
    Add JavaDoc explaining what the code does overall.

--- src/Java_SE_7/ArrayList.java
+++ src/Java_SE_7/ArrayList.java
@@ -1,5 +1,10 @@
 package Java_SE_7;
 
+/**
+ * @author arvindhvelrajan
+ * @param <E> Generic variable for this class
+ * @code This class adds all methods associated with ArrayList for Java SE 6.
+ */
 public class ArrayList
 {
 }
