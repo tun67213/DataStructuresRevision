726f44dc44ff511d286f9eece9ec1fff638200d8
Arvindh Velrajan <tun67213@temple.edu>
2026-03-26 20:17:06 +0900

    Extend dependency
    Extend Collection interface, whose methods Queue depends on.

--- src/Java_SE_7/Queue.java
+++ src/Java_SE_7/Queue.java
@@ -5,6 +5,6 @@ package Java_SE_7;
  * @param <E> Generic variable for this interface
  * @code This interface adds all methods associated with Queue for Java SE 7.
  */
-public interface Queue<E>
+public interface Queue<E> extends Collection<E>
 {
 }
