4da24b8ff5e30b1e96a6ba91ce99df9cb8dae820
Arvindh Velrajan <tun67213@temple.edu>
2026-03-27 19:52:10 +0900

    Import modules
    Import modules necessary for implementing class.

--- src/Java_SE_7/ArrayList.java
+++ src/Java_SE_7/ArrayList.java
@@ -5,6 +5,6 @@ package Java_SE_7;
  * @param <E> Generic variable for this class
  * @code This class adds all methods associated with ArrayList for Java SE 6.
  */
-public class ArrayList<E>
+public class ArrayList<E> extends AbstractList<E> implements List<E>, RandomAccess, Cloneable, Serializable
 {
 }
