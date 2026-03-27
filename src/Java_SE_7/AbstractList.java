c9b0d1f1b2e44e4d75fc8ed810b0d6aab8432036
Arvindh Velrajan <tun67213@temple.edu>
2026-03-27 19:11:08 +0900

    Import modules
    Import modules necessary for adding methods.

--- src/Java_SE_7/AbstractList.java
+++ src/Java_SE_7/AbstractList.java
@@ -6,6 +6,6 @@ package Java_SE_7;
  * @code This class adds all methods associated with Abstract List for Java SE 7.
  * @implNote None of the methods in this class will be implemented. Please feel free to implement them at your own interest if necessary.
  */
-public abstract class AbstractList<E>
+public abstract class AbstractList<E> extends AbstractCollection<E> implements List<E>
 {
 }
