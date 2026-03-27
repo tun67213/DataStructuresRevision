d27a3cfc9648a1ed7a56ed22b6db4c47d010593a
Arvindh Velrajan <tun67213@temple.edu>
2026-03-27 19:45:46 +0900

    Add methods
    Add all methods associated with it.

--- src/Java_SE_7/AbstractSequentialList.java
+++ src/Java_SE_7/AbstractSequentialList.java
@@ -8,5 +8,71 @@ package Java_SE_7;
  */
 public abstract class AbstractSequentialList<E> extends AbstractList<E>
 {
-	
+	/**
+	 * @code Sole constructor.
+	 */
+	protected AbstractSequentialList()
+	{
+
+	}
+
+	/**
+	 * @code Inserts the specified element at the specified position in this list (optional operation).
+	 */
+	@Override
+	public void add(int index, E element)
+	{
+		throw new UnsupportedOperationException("Will not be implemented.");
+	}
+
+	/**
+	 * @code Inserts all of the elements in the specified collection into this list at the specified position (optional operation).
+	 */
+	@Override
+	public boolean addAll(int index, Collection<? extends E> c)
+	{
+		throw new UnsupportedOperationException("Will not be implemented.");
+	}
+
+	/**
+	 * @code Returns the element at the specified position in this list.
+	 */
+	@Override
+	public E get(int index)
+	{
+		throw new UnsupportedOperationException("Will not be implemented.");
+	}
+
+	/**
+	 * @code Returns an iterator over the elements in this list (in proper sequence).
+	 */
+	@Override
+	public Iterator<E> iterator()
+	{
+		throw new UnsupportedOperationException("Will not be implemented.");
+	}
+
+	/**
+	 * @code Returns a list iterator over the elements in this list (in proper sequence).
+	 */
+	@Override
+	public abstract ListIterator<E> listIterator();
+
+	/**
+	 * @code Removes the element at the specified position in this list (optional operation).
+	 */
+	@Override
+	public E remove(int index)
+	{
+		throw new UnsupportedOperationException("Will not be implemented.");
+	}
+
+	/**
+	 * @code Replaces the element at the specified position in this list with the specified element (optional operation).
+	 */
+	@Override
+	public E set(int index, E element)
+	{
+		throw new UnsupportedOperationException("Will not be implemented.");
+	}
 }
