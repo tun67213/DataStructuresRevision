0b77d19132cb1a6d57102f83c2bb3fdad4860f78
Arvindh Velrajan <tun67213@temple.edu>
2026-03-30 17:42:25 +0900

    Add tags
    Add @SuppressWarnings tags to both toArray methods.

--- src/Java_SE_7/LinkedList.java
+++ src/Java_SE_7/LinkedList.java
@@ -1826,6 +1826,7 @@ public class LinkedList<E> extends AbstractSequentialList<E> implements List<E>,
 	/**
 	 * @return an array containing all of the elements in this list
 	 */
+	@SuppressWarnings("unchecked")
 	@Override
 	public Object[] toArray()
 	{
@@ -1838,6 +1839,7 @@ public class LinkedList<E> extends AbstractSequentialList<E> implements List<E>,
 	 * @throws NullPointerException if no array is provided
 	 * @code Returns an array containing all of the elements in this list in proper sequence (from first to last element); the runtime type of the returned array is that of the specified array.
 	 */
+	@SuppressWarnings("unchecked")
 	@Override
 	public <T> T[] toArray(T[] a)
 	{
